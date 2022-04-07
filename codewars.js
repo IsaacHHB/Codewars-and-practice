f9c7a8c5c4b5a3eadf6b712253fdb27ec1edca40
IsaacHHB <99921497+IsaacHHB@users.noreply.github.com>
2022-04-07 15:21:38 -0700

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -73,4 +73,10 @@ function boolToWord( bool ){
   }
 } 
 
+//Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+  return x.split(' ').join('')
+}
+
 
