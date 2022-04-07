ac4dcbc74c7bcbf0217eb21e0f80b0c6f4585467
IsaacHHB <99921497+IsaacHHB@users.noreply.github.com>
2022-04-07 15:21:15 -0700

    codewar

--- codewars.js
+++ codewars.js
@@ -62,3 +62,15 @@ function solution(str){
   let arr = str.split('').reverse().join('')
   return arr
 }
+
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  if(bool === true){
+    return 'Yes'
+  }else{
+    return 'No'
+  }
+} 
+
+
