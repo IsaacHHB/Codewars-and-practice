6050d8c692b30685c1df067fd5c37ff433a8a6de
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-09 15:33:38 -0700

    code war rounded century

--- codewars.js
+++ codewars.js
@@ -122,3 +122,9 @@ function isDivisible(n, x, y) {
     return false
   }
 }
+
+//The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+function century(year) {
+  return Math.ceil(year / 100);
+}
