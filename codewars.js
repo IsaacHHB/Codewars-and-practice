ea65d04e6da3539d3140657796d3b735d40da473
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-09 15:59:28 -0700

    reverse array codewar

--- codewars.js
+++ codewars.js
@@ -136,3 +136,9 @@ function century(year) {
 function litres(time) {
   return Math.floor(time/2);
 }
+
+//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+function digitize(num) {
+  return num.toString().split('').reverse().map(Number)
+}
