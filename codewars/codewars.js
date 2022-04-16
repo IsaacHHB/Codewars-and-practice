9b00ecb035d7cd76e9432b142e9335574e9689a7
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-16 16:44:57 -0700

    n power codewar

--- codewars/codewars.js
+++ codewars/codewars.js
@@ -216,3 +216,10 @@ function well(x){
       break;
   }
 }
+
+
+//You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+function index(array, n){
+  return isNaN(array[n]**n) ? -1 : array[n]**n
+}
