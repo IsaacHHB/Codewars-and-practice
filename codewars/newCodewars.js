52817c2e249004474a69ade4166f88e53e122bc2
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-19 16:02:47 -0700

    Update newCodewars.js

--- codewars/newCodewars.js
+++ codewars/newCodewars.js
@@ -73,4 +73,15 @@ function problem(x){
     return typeof x != "string" ? x*50+6 : "Error"
   }
 
+//Return the number (count) of vowels in the given string. We will consider a, e, i, o, u as vowels for this Kata (but not y). The input string will only consist of lower case letters and/or spaces.
 
+function getCount(str) {
+    var vowelsCount = 0;
+    const vowels = ['a','e','i','o','u']
+    for(let char of str){
+      if(vowels.includes(char)){
+        vowelsCount++
+      }
+    }
+    return vowelsCount;
+  }
