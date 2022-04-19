0c9b6377eddc334b299bac60c4a3d3d4b08dd06c
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-19 16:02:14 -0700

    Update newCodewars.js

--- codewars/newCodewars.js
+++ codewars/newCodewars.js
@@ -46,3 +46,13 @@ function greet(name){
     return (n*2) -2
   }
 
+//Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+
+function generateRange(min, max, step){
+    let array = []
+    for(i = min; i <= max; i+=step){
+      array.push(i)
+    }
+    return array
+  }
+
