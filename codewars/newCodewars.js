1a46813c272d46529abd82a1f3139d7e5feefe79
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-20 14:58:54 -0700

    Update newCodewars.js

--- codewars/newCodewars.js
+++ codewars/newCodewars.js
@@ -105,3 +105,8 @@ function makeUpperCase(str) {
     return str.toUpperCase()
   }
 
+//Build a function that returns an array of integers from n to 1 where n>0.
+
+const reverseSeq = n => {
+    return Array(n).fill(0).map((e, i) => n - i );
+  };
