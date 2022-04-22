fd7450093e0b6809f3f811dc312abbae4bc4db6d
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-22 15:59:43 -0700

    Create MagicEightBall.js

---
+++ codewars/MagicEightBall.js
@@ -0,0 +1,35 @@
+let userName = 'Jane'
+userName === 'Jane' ? console.log(`Hello ${userName}`) : console.log(`Hello!`)
+
+userQuestion = 'Will I die old?'
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+switch (randomNumber) {
+  case 0:
+    eightBall = "It is certain";
+    break;
+  case 1:
+    eightBall = "It is decidedly so";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    eightBall = "Do not count on it";
+    break;
+  case 5:
+    eightBall = "My sources say no";
+    break;
+  case 6:
+    eightBall = "Outlook not so good";
+    break;
+  case 7:
+    eightBall = "Signs point to yes";
+    break;
+}
+console.log(`${userName} would like to know ${userQuestion}...... the magic eight ball says ${eightBall}`)
