981e4a2e04326b964f165432a543217236402651
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-23 17:34:41 -0700

    Update newCodewars.js

--- codewars/newCodewars.js
+++ codewars/newCodewars.js
@@ -136,3 +136,15 @@ const reverseSeq = n => {
     //TODO : calculate the downward rounded average of the marks array
     return Math.floor(marks.reduce((x,y) => x + y) / marks.length);
   }
+
+
+//A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+function hero(bullets, dragons){
+    bulletNeeded = dragons * 2
+    if(bullets >= bulletNeeded){
+      return true
+    }else{
+      return false
+    }
+  }
