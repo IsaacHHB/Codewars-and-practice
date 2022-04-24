75c20d077ae9bc120e98cc231f71ee107fc57518
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-24 14:43:05 -0700

    Update newCodewars.js

--- codewars/newCodewars.js
+++ codewars/newCodewars.js
@@ -148,3 +148,9 @@ function hero(bullets, dragons){
       return false
     }
   }
+
+//Write a function which calculates the average of the numbers in a given list.
+
+function find_average(array) {
+    return  array.reduce((a,b) => a + b) / array.length
+}
