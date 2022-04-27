89bff94de35b61c5443ecf8367b3a616c7b7780b
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-27 12:29:17 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -3,3 +3,16 @@
 function arrayPlusArray(arr1, arr2) {
     return arr1.reduce((a,b)=> a + b, 0) + arr2.reduce((a,b)=> a + b, 0); 
   }
+
+//Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+  function countPositivesSumNegatives(input) {
+    let positiveNums = 0;
+    let negativeNums = 0;
+    if (input === null || input.length === 0) {
+      return [];
+    } else {
+      input.forEach((num) => num > 0 ? positiveNums++ : negativeNums += num);
+    }
+    return [positiveNums , negativeNums];
+}
