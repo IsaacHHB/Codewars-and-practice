4c2c378ced34c4237fb4386b61f7f166ca661f07
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-28 13:47:54 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -76,3 +76,10 @@ function stringToArray(string){
     return string.split(' ')
   }
 
+//Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+function greet (name, owner) {
+    return name === owner ? `Hello boss` : `Hello guest`
+}
+
+
