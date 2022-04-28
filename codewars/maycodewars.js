83dcfa5713b1f327646f57493f49e038a4da2d44
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-28 13:47:22 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -58,3 +58,15 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     return distanceToPump <= mpg * fuelLeft
   };
 
+  //Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+
+var min = function(list){
+    
+    return Math.min(...list);
+}
+
+var max = function(list){
+    
+    return Math.max(...list);
+}
+
