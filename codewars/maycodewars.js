4a8225821736720157fb3df352ca2a439d808bfb
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-28 13:46:34 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -36,3 +36,19 @@ function monkeyCount(n) {
     }
     return arr
   }
+
+//Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ). The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+const sumArray = array => {
+    if (!array) {
+        return 0
+    }
+    let s = 0;
+    array.sort((a, b) => a - b).forEach((a, i) => {
+        if (i !== array.length - 1 && i !== 0) {
+            s += a;
+        }
+    })
+    return s;
+}
+
