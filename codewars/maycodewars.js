8ba4bc4849f75f4a945238c1c2fb3766b1df13b0
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-29 18:54:36 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -104,5 +104,7 @@ function sumMix(x){
 
 //Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
-
+function removeExclamationMarks(s) {
+    return s.replace(/!/g,'');
+}
 
