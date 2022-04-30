da2cead88cd08b79f6eb201c8cf272b1d357fd2a
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-29 18:53:07 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -82,4 +82,6 @@ function greet (name, owner) {
     return name === owner ? `Hello boss` : `Hello guest`
 }
 
+//Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
 
