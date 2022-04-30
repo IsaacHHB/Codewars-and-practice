e9e34ab8fe35e32598f7ab843649755c85506525
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-29 18:53:34 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -91,4 +91,6 @@ var countSheep = function (num){
     }
     return result
   }
+//Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
 
