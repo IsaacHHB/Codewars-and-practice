4f56457f473de02c8bde6e431d81bf08d9e6ba23
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-01 00:58:14 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -110,3 +110,12 @@ function removeExclamationMarks(s) {
 
 // The manager of the car rental makes you some good offers. Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total. Write a code that gives out the total amount for different days(d).
 
+function rentalCarCost(d) {
+  if(d >= 7){
+    return (d * 40) - 50
+  }else if(d >= 3){
+    return (d * 40) - 20
+  }else{
+    return d * 40
+  }
+}
