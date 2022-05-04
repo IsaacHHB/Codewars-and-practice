803d4559ae165758d46ccbb34bb12c6b55cdb002
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-03 20:38:30 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -125,3 +125,6 @@ function rentalCarCost(d) {
 const quarterOf = (month) => {
   return month >= 10 ? 4 : month >= 7 ? 3 : month >= 4 ? 2 : 1 
 }
+
+
+//Create a function with two arguments that will return an array of the first (n) multiples of (x). Assume both the given number and the number of times to count will be positive numbers greater than 0.Return the results as an array (or list in Python, Haskell or Elixir).
