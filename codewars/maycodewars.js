dc7b909ee8273b46903ecb82c0f6a35ec5caac03
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-03 20:38:39 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -128,3 +128,11 @@ const quarterOf = (month) => {
 
 
 //Create a function with two arguments that will return an array of the first (n) multiples of (x). Assume both the given number and the number of times to count will be positive numbers greater than 0.Return the results as an array (or list in Python, Haskell or Elixir).
+function countBy(x, n) {
+  let z = [];
+  const limit = x * n
+  for (let i = 1; i <= limit; i++){
+    if (i % x === 0) z.push(i)
+  }
+  return z;
+}
