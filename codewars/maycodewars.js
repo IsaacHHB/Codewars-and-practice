f09522c1548da8121ea24000d953e226856e72e8
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-06 13:12:07 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -191,7 +191,7 @@ function getPlanetName(id){
   return name;
 }
 
-
+//You are given two interior angles (in degrees) of a triangle. Write a function to return the 3rd. Note: only positive integers will be tested.
 
 function otherAngle(a, b) {
   return 180 - a - b
