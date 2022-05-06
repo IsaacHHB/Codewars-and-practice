d1f25955760716f6fd94e04c8e5734cdac572b00
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-06 13:58:44 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -204,3 +204,12 @@ function feast(beast, dish) {
 }
 
 //Your task is to find the first element of an array that is not consecutive. By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+function firstNonConsecutive (arr) {
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i + 1] - arr[i] !==1){
+      return arr[i + 1];
+    }
+  }
+   return null
+ }
