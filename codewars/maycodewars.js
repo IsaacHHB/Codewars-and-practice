1afd976cb16d2eb1cf0018ae657bc5138830840f
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-07 15:42:05 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -213,3 +213,5 @@ function firstNonConsecutive (arr) {
   }
    return null
  }
+
+ //Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
