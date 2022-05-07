de85ccc5498db2b749b17ddf626561f429693c4e
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-07 16:58:13 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -229,3 +229,5 @@ function firstNonConsecutive (arr) {
     return 'F'
   }
 }
+
+//Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
