1a51085004ec61527053f60ac8bf32cefce5e2ed
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-07 15:45:25 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -215,3 +215,17 @@ function firstNonConsecutive (arr) {
  }
 
  //Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+ function getGrade (s1, s2, s3) {
+  if((s1 + s2 +s3) / 3 >= 90){
+    return 'A'
+  }else if((s1 + s2 +s3) / 3 >= 80){
+    return 'B'
+  }else if((s1 + s2 +s3) / 3 >= 70){
+    return 'C'
+  }else if((s1 + s2 +s3) / 3 >= 60){
+    return 'D'
+  }else{  
+    return 'F'
+  }
+}
