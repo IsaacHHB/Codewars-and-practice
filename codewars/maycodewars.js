9d6db205abe011c043b9e5c3ea8892bace808dce
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-08 13:26:56 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -237,3 +237,12 @@ function setAlarm(employed, vacation){
 }
 
 //Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+function doubleChar(str) {
+  let doubleChar = ''
+  
+  for (let i = 0 ; i < str.length; i++){
+    doubleChar += str[i] + str[i]
+  }
+  return doubleChar
+}
