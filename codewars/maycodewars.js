a58dd1feb91440e8b3a0c54d1d7354d7651e323f
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-09 13:50:34 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -260,3 +260,5 @@ function enough(cap, on, wait) {
     return (cap - on - wait) * -1 
   }
 }
+
+//Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
