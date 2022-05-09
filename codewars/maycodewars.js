9acd4ee3667bf20b6b6afcfaa0cd39a6b567c465
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-09 12:54:43 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -252,3 +252,11 @@ function doubleChar(str) {
 const removeEveryOther = array => array.filter((_, i) => i % 2 === 0)
 
 //Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
+function enough(cap, on, wait) {
+  if(on + wait <= cap){
+    return 0
+  }else{
+    return (cap - on - wait) * -1 
+  }
+}
