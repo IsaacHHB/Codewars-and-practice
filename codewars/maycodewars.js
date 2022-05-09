f10c5619c24f9013b6a1d43a9749544d06c2c306
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-09 13:50:50 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -262,3 +262,6 @@ function enough(cap, on, wait) {
 }
 
 //Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+
+const divisibleBy = (numbers, divisor) =>
+  numbers.filter(number => number % divisor === 0)
