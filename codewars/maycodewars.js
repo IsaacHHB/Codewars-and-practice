e2cc3af3bc328df30f91af6bf3348eea26cd7cce
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-12 18:15:58 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -284,3 +284,5 @@ const divisibleBy = (numbers, divisor) =>
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
+
+//Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
