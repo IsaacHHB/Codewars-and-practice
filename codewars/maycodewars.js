7f94b7ec742f74d7fa9fb57784b90e989e05919d
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-14 06:48:12 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -340,3 +340,5 @@ function hoopCount (n) {
     return `Great, now move on to tricks`
   }  
 }
+
+//Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, () In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
