ca076802f67c80ef9f2ae5095579a69781609259
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-14 06:48:34 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -342,3 +342,5 @@ function hoopCount (n) {
 }
 
 //Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, () In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+
+const expressionMatter = (a, b, c) => Math.max(a + b + c, a * b * c, (a + b) * c, a * (b + c))
