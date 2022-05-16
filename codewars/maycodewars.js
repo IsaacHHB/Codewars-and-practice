8c9279e45992b81ca1ba13bd5d2eeb4483334f26
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-16 15:28:25 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -362,3 +362,9 @@ function createArray(number){
 const areaOrPerimeter = (l , w) => l === w ? l*w : 2*(l+w);
 
 //Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+}
