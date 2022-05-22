833adb14b86dbf0b1255abb9213225356b2749b5
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-21 21:18:36 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -387,3 +387,19 @@ function twoSort(s) {
 }
 
 //Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+function howMuchILoveYou(nbPetals) {
+  if(nbPetals%6 == 1){
+  return ("I love you");
+}else if(nbPetals%6 == 2){
+  return ("a little");
+}else if(nbPetals%6 ==3){
+  return ("a lot");
+}else if(nbPetals%6 ==4){
+  return ("passionately");
+}else if(nbPetals%6 == 5){
+  return ("madly");
+}else{
+  return ("not at all");
+}
+}
