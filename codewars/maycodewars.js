9281f5e59f5e465c4741e5234b89a03f02a49f96
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-22 14:23:16 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -409,3 +409,5 @@ function howMuchILoveYou(nbPetals) {
 function strCount(str, letter){  
   return str.split('').filter((x) => letter === x).length
 }
+
+//Write a method, that will get an integer array as parameter and will process every number from this array. Return a new array with processing every number of the input-array like this: If the number has an integer square root, take this, otherwise square the number.
