d16fffb0fa99c53f199acc6e2ae7ef11bdf4d419
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-22 14:23:33 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -411,3 +411,7 @@ function strCount(str, letter){
 }
 
 //Write a method, that will get an integer array as parameter and will process every number from this array. Return a new array with processing every number of the input-array like this: If the number has an integer square root, take this, otherwise square the number.
+
+function squareOrSquareRoot(array) {
+  return array.map(i => Math.sqrt(i) == Math.sqrt(i).toFixed(0) ? Math.sqrt(i) : i * i)  
+}
