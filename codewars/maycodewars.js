e689e1a31826df696bba4c5c58395e92b448fbc2
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-23 13:16:06 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -421,3 +421,5 @@ function squareOrSquareRoot(array) {
 const dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
 
 const  howManyDalmatians = number => number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number === 101 ? dogs[3] : dogs[2];
+
+//We want an array, but not just any old array, an array with contents! Write a function that produces an array with the numbers 0 to N-1 in it.
