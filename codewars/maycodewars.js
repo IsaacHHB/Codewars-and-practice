03c8865ed40475e551143267c1d6d2749c9fe1ca
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-23 13:42:51 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -431,3 +431,5 @@ const arr = N => {
   }
   return arr;
 };
+
+//Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them. Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
