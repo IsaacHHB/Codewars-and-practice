9578f6f9567fe1b5b7dbb505d5504decf4765dc7
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-23 13:09:08 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -415,3 +415,5 @@ function strCount(str, letter){
 function squareOrSquareRoot(array) {
   return array.map(i => Math.sqrt(i) == Math.sqrt(i).toFixed(0) ? Math.sqrt(i) : i * i)  
 }
+
+//Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, and you simply do not know how to respond! By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has. The number of dogs will always be a number and there will always be at least 1 dog.
