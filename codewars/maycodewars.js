791001cc289e0befc68a83a518559306af77dad5
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-25 14:26:12 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -444,3 +444,7 @@ function warnTheSheep(queue) {
 }
 
 //Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+function sumStr(a,b) {
+  return String(Number(a) + Number(b))
+}
