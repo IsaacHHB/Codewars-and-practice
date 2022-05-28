4c6d6e89c37511097ce22f64d66c69fee311457d
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-28 16:48:38 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -492,3 +492,8 @@ function saleHotdogs(n){
 }
 
 //In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+function filter_list(l) {
+  const onlyNums = l.filter(e => typeof e === 'number')
+  return onlyNums
+}
