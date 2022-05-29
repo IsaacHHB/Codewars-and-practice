00faa9c9ab3168ac1f630f86471307d460143d40
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-29 10:43:52 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -509,4 +509,7 @@ function isPalindrome(x) {
 function unusualFive() {
   let five = 'Five!';
   return five.length;
-  }
+}
+
+//Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String). Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
