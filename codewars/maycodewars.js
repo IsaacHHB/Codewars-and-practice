c9e3fbd8048d73b238da347b8ea7afc4547a1574
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-30 18:37:51 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -520,3 +520,6 @@ function hello(name) {
     return `Hello, ${name[0].toUpperCase()}${name.substring(1,).toLowerCase()}!`
   }
 }
+
+//Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below! E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
