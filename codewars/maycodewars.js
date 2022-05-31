6e92927aa638afe6d057c1444e0067a423be3548
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-05-30 18:38:12 -0700

    Update maycodewars.js

--- codewars/maycodewars.js
+++ codewars/maycodewars.js
@@ -523,3 +523,11 @@ function hello(name) {
 
 //Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below! E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
 
+function tripleTrouble(one, two, three){
+  const arr = []
+  
+  for(let i = 0; i < one.length; i++){
+    arr.push(one[i],two[i],three[i])
+  }
+  return arr.join('')
+ }
