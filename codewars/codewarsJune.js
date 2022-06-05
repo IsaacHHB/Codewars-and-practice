1226c9daacf501aa0d43a6e46ec8e945d482919e
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-05 12:23:12 -0700

    new month new codewars

---
+++ codewars/codewarsJune.js
@@ -0,0 +1,1 @@
+//Looks like some hoodlum plumber and his brother has been running around and damaging your stages again. The pipes connecting your level's stages together need to be fixed before you receive any more complaints. Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
