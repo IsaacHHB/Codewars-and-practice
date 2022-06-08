456acdde9ae7bfb2ca592b712e11fa2ddc921152
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-08 12:54:22 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -8,7 +8,7 @@ function pipeFix(numbers){
     return arr
   }
 
-//
+//Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
 
 function combat(health, damage) {
   return Math.max(health - damage, 0);
