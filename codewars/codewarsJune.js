7d43cdc0479f3f19095ae5fa3fb58e00c4340056
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-11 15:32:22 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -19,3 +19,5 @@ function combat(health, damage) {
 function nameShuffler(str){
   return str.split(' ').reverse().join(' ')
 }
+
+//This function should test if the factor is a factor of base. Return true if it is a factor or false if it is not.
