22cf284f50fcaac1c4486389a20cc9a24efb9772
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-11 15:32:37 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -21,3 +21,7 @@ function nameShuffler(str){
 }
 
 //This function should test if the factor is a factor of base. Return true if it is a factor or false if it is not.
+
+function checkForFactor (base, factor) {
+  return base % factor === 0 ? true : false
+}
