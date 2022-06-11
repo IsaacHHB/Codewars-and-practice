f2f6af97bd3614e9c827329f36ff8b7ab2aa8466
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-11 15:29:41 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -15,3 +15,7 @@ function combat(health, damage) {
 }
 
 //Write a function that returns a string in which firstname is swapped with last name.
+
+function nameShuffler(str){
+  return str.split(' ').reverse().join(' ')
+}
