550e31ab3fc3068fb03441649a6db999e69061ae
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-13 16:05:57 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -61,3 +61,7 @@ function peopleWithAgeDrink(old) {
 
 //Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
 
+function replace(s){
+  return s.replace(/[aeiou]/gi,'!')
+}
+
