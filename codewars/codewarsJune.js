dda2b92ad44605e78ce3e4ad1031461d4cffcd44
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-13 16:05:41 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -55,3 +55,7 @@ function convertToCelsius (temperature) {
 
 //Drink about kata
 
+function peopleWithAgeDrink(old) {
+  return old <= 13 ? "drink toddy" : old <= 17 ? "drink coke" : old <= 20 ? "drink beer": "drink whisky"
+};
+
