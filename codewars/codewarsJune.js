d0322b788571bb0b27c512365b62bbd8ae80be60
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-12 18:08:26 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -25,3 +25,5 @@ function nameShuffler(str){
 function checkForFactor (base, factor) {
   return base % factor === 0 ? true : false
 }
+
+//The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
