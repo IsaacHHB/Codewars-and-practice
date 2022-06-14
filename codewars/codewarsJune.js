0eb7ff194801c0f37af3f61fc46f630f95fc2b47
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-14 13:37:42 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -85,3 +85,9 @@ function descendingOrder(n){
   return Number(num)
 }
 
+// This time no story, no theory. The examples below show you how to write function accum:
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
