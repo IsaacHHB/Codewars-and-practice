16347facb9bbe9d2f5e9b1d4e317772571b1b711
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-14 13:37:25 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -78,3 +78,5 @@ function highAndLow(numbers){
   return arr[0] + " " + arr[arr.length - 1]
 }
 
+//Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
