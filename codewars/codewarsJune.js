a2bd33c967951dc24e6b3a79b42f3c77ba96a2fe
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-14 13:37:16 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -73,3 +73,8 @@ function reverseList(list) {
 
 //In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
+function highAndLow(numbers){
+  const arr = numbers.split(" ").map(Number).sort((a,b) => b - a)
+  return arr[0] + " " + arr[arr.length - 1]
+}
+
