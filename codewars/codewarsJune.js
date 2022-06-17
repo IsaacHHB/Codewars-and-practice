db8e5190834cba1f6d40716aab124c61d72b65ab
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-17 15:06:09 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -120,3 +120,7 @@ function remove (s) {
 }
 
 //Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+function getChar(c) {
+  return String.fromCharCode(c)
+}
