08b90f33c4b0deca1babe78b676c2a53a7832a54
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-20 15:46:12 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -166,3 +166,7 @@ function noBoringZeros(n) {
 
 //Merge two sorted arrays into one
 
+function mergeArrays(arr1, arr2) {
+  return Array.from(new Set(arr1.concat(arr2))).sort((a,b) => a - b); 
+}
+
