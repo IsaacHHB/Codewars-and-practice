a5a909b62bdaa47e82f0e1867178afdb0093dd89
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-20 15:45:47 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -160,3 +160,7 @@ function mouthSize(animal) {
 //No zeros for heros
 //Numbers ending with zeros are boring. They might be fun in your world, but not here. Get rid of them. Only the ending ones.
 
+function noBoringZeros(n) {
+  return +`${n}`.replace(/0*$/, "");
+}
+
