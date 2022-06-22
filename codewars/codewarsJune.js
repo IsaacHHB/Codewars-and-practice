5ae66f0086be017139a6405c57e3386234fcd327
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-22 12:54:10 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -223,3 +223,6 @@ function hexToDec(hexString){
 //Bin to Decimal
 //Complete the function which converts a binary number (given as a string) to a decimal number.
 
+function binToDec(bin){
+  return parseInt(bin,2);
+}
