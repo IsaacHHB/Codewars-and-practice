eedb152e4352ca8a93a506b3f2bca7f6979e699b
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-24 16:28:21 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -237,3 +237,7 @@ function take(arr, n) {
 //Failed Sort - Bug Fixing #4
 //Oh no, Timmy's Sort doesn't seem to be working? Your task is to fix the sortArray function to sort all numbers in ascending order
 
+var sortArray = function(value) {
+  return value.split('').sort((c, p) => c - p ).join('');
+}
+
