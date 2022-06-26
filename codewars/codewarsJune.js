0454550690cc81e3e87236321968806a9c70d12d
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-26 14:38:23 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -278,3 +278,8 @@ function evalObject(value){
 
 //SpeedCode #2 - Array Madness
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+function arrayMadness(a, b) {
+  return a.reduce((acc, cur) => acc + (cur ** 2), 0) > b.reduce((acc, cur) => acc + (cur ** 3), 0);
+};
+
