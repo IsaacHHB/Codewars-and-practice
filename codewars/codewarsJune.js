ba9c2a6d3f0c33de0c5127633195cc9db40528e1
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-25 17:45:52 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -242,3 +242,5 @@ var sortArray = function(value) {
 }
 
 //String Templates - Bug Fixing #5
+//Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
