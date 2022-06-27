a31a9fd603514ed7c9ba484b192ec15f398ac76e
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-06-27 14:38:52 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -284,3 +284,5 @@ function arrayMadness(a, b) {
 };
 
 //Pre-FizzBuzz Workout #1
+//This is the first step to understanding FizzBuzz. Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value. Your expected output is an array of positive integers from 1 to n (inclusive). Your job is to write an algorithm that gets you from the input to the output.
+
