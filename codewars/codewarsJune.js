4317b299db946a05762bdbc9dfb52790fd39e89f
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-07-10 01:45:19 -0700

    Update codewarsJune.js

--- codewars/codewarsJune.js
+++ codewars/codewarsJune.js
@@ -348,3 +348,5 @@ function position(letter){
   let alphabet = ['zero trolls', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   return `Position of alphabet: ${alphabet.indexOf(letter)}`;
 }
+
+//How old will I be in 2099?
