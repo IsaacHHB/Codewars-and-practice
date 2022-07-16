8b0bf43966226c84965d76d55dd672320f85cbf4
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-07-16 14:02:27 -0700

    Update codewarsJuly.js

--- codewars/codewarsJuly.js
+++ codewars/codewarsJuly.js
@@ -48,3 +48,8 @@ function stringClean(s){
 //Shortest Word
 //Simple, given a string of words, return the length of the shortest word(s). String will never be empty and you do not need to account for different data types.
 
+function findShort(s){
+  let str = s.split(' ')
+  return Math.min(...str.map(({ length }) => length))
+}
+
