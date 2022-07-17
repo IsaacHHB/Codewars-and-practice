f19bac0bf21bfbf4962cb35131b0b783e446a90b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-07-17 12:11:48 -0700

    Update codewarsJuly.js

--- codewars/codewarsJuly.js
+++ codewars/codewarsJuly.js
@@ -81,3 +81,13 @@ function pigIt(str){
 
 //Palindrome Strings
 //A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This includes capital letters, punctuation, and word dividers. Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first.
+
+function isPalindrome(line) {
+  let pali = line.toString().split('').reverse().join('');
+  if(pali != line){
+    return false
+  }else{
+    return true
+  }
+}
+
