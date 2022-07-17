26c4eaa6d071501957e3faaf49d24fb0b40120d8
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-07-17 12:12:03 -0700

    Update codewarsJuly.js

--- codewars/codewarsJuly.js
+++ codewars/codewarsJuly.js
@@ -92,3 +92,5 @@ function isPalindrome(line) {
 }
 
 //Moving Zeros To The End
+//Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
