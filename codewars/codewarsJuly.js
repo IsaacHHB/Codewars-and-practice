8c95db6936639b3b23888c47c93a27227ba5193b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-07-17 12:11:25 -0700

    Update codewarsJuly.js

--- codewars/codewarsJuly.js
+++ codewars/codewarsJuly.js
@@ -73,3 +73,9 @@ function getMiddle(s){
 //Simple Pig Latin
 //Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
+function pigIt(str){
+  let marks = ['.','!','?','"', ',']
+  let result = str.split(' ').map(word => word.slice(1) + word[0] + (marks.includes(word) ? '' : 'ay'));
+  return result.join(' ')
+}
+
