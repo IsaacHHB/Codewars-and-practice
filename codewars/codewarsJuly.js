7062e47ad1c447078a704af6b5586c28015cdad3
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-07-23 16:08:23 -0700

    Update codewarsJuly.js

--- codewars/codewarsJuly.js
+++ codewars/codewarsJuly.js
@@ -112,3 +112,5 @@ function sayHello( name, city, state ) {
 
 //Grasshopper - Array Mean
 //Find the mean (average) of a list of numbers in an array.
+//Information
+//To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
