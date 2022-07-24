7d90de40e54a71d8a5ed4ca44707476601043446
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-07-24 16:08:43 -0700

    Update codewarsJuly.js

--- codewars/codewarsJuly.js
+++ codewars/codewarsJuly.js
@@ -114,3 +114,5 @@ function sayHello( name, city, state ) {
 //Find the mean (average) of a list of numbers in an array.
 //Information
 //To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+//For an example list of 1, 3, 5, 7
+//1. Add all of the numbers
