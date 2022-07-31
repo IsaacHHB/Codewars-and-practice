ee7a05bfaf73951d223f522f3cb95a58a98481f5
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-07-31 16:15:55 -0700

    Update codewarsJuly.js

--- codewars/codewarsJuly.js
+++ codewars/codewarsJuly.js
@@ -130,3 +130,7 @@ var findAverage = function (nums) {
 //You can define the object attributes during initialization, like this:
 //var animal={name:"dog"}
 //you can also set/get some properties after the object definition, like this:
+//var animal={}
+// animal.name="dog"  (or animal["name"]="dog")
+// Task
+// Give you a function animal, accept 1 parameter:obj like this:
