d9d3852dc1ddc635a4b46d9073e4ebc3c34b7516
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-02 16:17:08 -0700

    Update codewarsJuly.js

--- codewars/codewarsJuly.js
+++ codewars/codewarsJuly.js
@@ -137,3 +137,7 @@ var findAverage = function (nums) {
 //{name:"dog",legs:4,color:"white"}
 // and return a string like this:
 // "This white dog has 4 legs."
+
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+}
