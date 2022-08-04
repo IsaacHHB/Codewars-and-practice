629569b43bf7eb862b7de18f269d209bd35a323c
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-03 19:32:55 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -12,3 +12,4 @@ function getSum(a,b){
   }
 
   //Sum of two lowest positive integers
+  //Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
