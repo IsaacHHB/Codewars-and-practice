59773a3521e912b28bfb1760b43c11e817171d32
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-03 19:33:20 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -1,15 +1,20 @@
 //Beginner Series #3 Sum of Numbers
 //Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
 
-function getSum(a,b){
-    if (a == b){
-      return a;
-    }else if (a > b){
-      return a + getSum(a-1,b)
+function getSum(a, b) {
+    if (a == b) {
+        return a;
+    } else if (a > b) {
+        return a + getSum(a - 1, b)
     } else {
-      return a + getSum(a+1,b)
+        return a + getSum(a + 1, b)
     }
-  }
+}
 
-  //Sum of two lowest positive integers
-  //Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+//Sum of two lowest positive integers
+//Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+function sumTwoSmallestNumbers(numbers) {
+    let res = numbers.sort((a, b) => a - b).slice(0, 2);
+    return res.reduce((e, i) => e + i)
+}
