4ad57054d8717453731f6d67d3ae3f04ff1fc99f
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-04 16:03:54 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -27,3 +27,4 @@ const flip=(d, a)=>{
   }
 
 //Regular Ball Super Ball
+//Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated. If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
