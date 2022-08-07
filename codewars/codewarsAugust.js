01fc895ffac092188f5af034aa3c487410b4ceac
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-07 11:37:36 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -39,3 +39,7 @@ var Ball = function (ballType) {
 
 //Sum of odd numbers
 //Given the triangle of consecutive odd numbers: Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+function rowSumOddNumbers(n) {
+    return n * n * n
+}
