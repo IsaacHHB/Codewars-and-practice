640a6409711373aea810727e8d02aee9a9a1c4cb
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-13 15:31:16 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -74,3 +74,22 @@ function calculateTip(amount, rating) {
 // L1: Bartender, drinks!
 //Complete the function that receives as input a string, and produces outputs according to the following table:
 //Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer". Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+function getDrinkByProfession(param){
+    param = param.toLowerCase()
+    if (param === "jabroni"){
+        return "Patron Tequila"
+    }else if (param === "school counselor"){
+        return "Anything with Alcohol"
+    }else if (param === "programmer"){
+        return "Hipster Craft Beer"
+    }else if (param === "bike gang member"){
+        return "Moonshine"
+    }else if (param === "politician"){
+        return "Your tax dollars"
+    }else if (param === "rapper"){
+        return "Cristal"
+    }else {
+      return "Beer"
+    }
+}
