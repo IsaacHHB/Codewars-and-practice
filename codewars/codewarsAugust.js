0ece102ee968ffd2b7fa4dedcd3969399e6e4f90
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-14 13:14:35 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -100,3 +100,5 @@ function getDrinkByProfession(param){
 function derive(coefficient,exponent) {
     return `${coefficient * exponent}x^${exponent - 1}`
 }
+
+//Exclamation marks series #2: Remove all exclamation marks from the end of sentence
