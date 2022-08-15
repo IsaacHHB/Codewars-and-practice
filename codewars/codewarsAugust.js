43c521d79d79137920b7978a80fef54e67db5209
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-15 13:25:06 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -75,29 +75,29 @@ function calculateTip(amount, rating) {
 //Complete the function that receives as input a string, and produces outputs according to the following table:
 //Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer". Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
 
-function getDrinkByProfession(param){
+function getDrinkByProfession(param) {
     param = param.toLowerCase()
-    if (param === "jabroni"){
+    if (param === "jabroni") {
         return "Patron Tequila"
-    }else if (param === "school counselor"){
+    } else if (param === "school counselor") {
         return "Anything with Alcohol"
-    }else if (param === "programmer"){
+    } else if (param === "programmer") {
         return "Hipster Craft Beer"
-    }else if (param === "bike gang member"){
+    } else if (param === "bike gang member") {
         return "Moonshine"
-    }else if (param === "politician"){
+    } else if (param === "politician") {
         return "Your tax dollars"
-    }else if (param === "rapper"){
+    } else if (param === "rapper") {
         return "Cristal"
-    }else {
-      return "Beer"
+    } else {
+        return "Beer"
     }
 }
 
 //Take the Derivative
 //This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent. Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to print out an expression (like 28x^7). "^1" should not be truncated when exponent = 2. In this case, the function should multiply 7 and 8, and then subtract 1 from 8. It should output "56x^7", the first number 56 being the product of the two numbers, and the second number being the exponent minus 1.
 
-function derive(coefficient,exponent) {
+function derive(coefficient, exponent) {
     return `${coefficient * exponent}x^${exponent - 1}`
 }
 
@@ -106,15 +106,23 @@ function derive(coefficient,exponent) {
 
 function remove(inputString) {
     let result = inputString;
-  
+
     // run this loop while the last character is a `!`
     while (result[result.length - 1] === "!") {
-      // remove the last character (= `!`)
-      result = result.slice(0, -1);
+        // remove the last character (= `!`)
+        result = result.slice(0, -1);
     }
-  
+
     return result;
-  }
+}
 
 //Delete occurrences of an element if it occurs more than n times
 //Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motif usually repeats. He isn't fond of seeing the Eiffel tower 40 times. He tells them that he will only sit for the session if they show the same motif at most N times. Luckily, Alice and Bob are able to encode the motif as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+
+function deleteNth(arr, x) {
+    var cache = {};
+    return arr.filter(function (n) {
+        cache[n] = (cache[n] || 0) + 1;
+        return cache[n] <= x;
+    });
+}
