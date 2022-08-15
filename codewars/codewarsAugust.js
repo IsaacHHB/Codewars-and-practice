56f161cf8dc72e3d81879ab80e58a0b085e7081d
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-15 13:15:03 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -103,3 +103,15 @@ function derive(coefficient,exponent) {
 
 //Exclamation marks series #2: Remove all exclamation marks from the end of sentence
 //Remove all exclamation marks from the end of sentence.
+
+function remove(inputString) {
+    let result = inputString;
+  
+    // run this loop while the last character is a `!`
+    while (result[result.length - 1] === "!") {
+      // remove the last character (= `!`)
+      result = result.slice(0, -1);
+    }
+  
+    return result;
+  }
