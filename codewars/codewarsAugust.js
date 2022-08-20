c513afaade0e499264cf12212b46a249aa177d26
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-20 16:30:00 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -128,7 +128,7 @@ function deleteNth(arr, x) {
 }
 
 //Is this a triangle?
-
+//Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
 
 
 
