70182e823db74d294f30d1a35aee516686ecd65f
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-20 16:46:58 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -142,4 +142,9 @@ multipleOfIndex = array => array.filter((e,i) => e % i == 0)
 //Surface Area and Volume of a Box
 //Write a function that returns the total surface area and volume of a box as an array: [area, volume]
 
+const getSize = (width, height, depth) => [
+    ((width * height) + (width * depth) + (depth * height)) * 2,
+    width * height * depth
+  ];
+
 
