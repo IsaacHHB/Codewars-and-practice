96908019237843f118e1c05f44675dd808a846b0
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-21 16:18:56 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -150,3 +150,7 @@ const getSize = (width, height, depth) => [
 //Alan Partridge II - Apple Turnover
 //Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
 
+function apple(x) {
+    return (x * x) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.'
+}
+
