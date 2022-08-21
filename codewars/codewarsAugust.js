76a8c87209d2d69610b1f936b5bf2df68226613f
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-21 16:19:27 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -157,3 +157,7 @@ function apple(x) {
 //Enumerable Magic #3 - Does My List Include This?
 //Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
 
+function include(arr, item) {
+    return arr.includes(item);
+}
+
