7e6365e009d339a6fbeeeed8486cb786282daea9
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-23 13:47:14 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -251,3 +251,5 @@ function fizzBuzz(x) {
 fizzBuzz(100)
 
 //Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right
+//Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
