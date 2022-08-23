afaf1d7d762f5f311a020353aec78cc5d79dc59b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-23 13:45:29 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -216,3 +216,12 @@ function subt(a, b) {
 //Sum of differences in array
 //Your task is to sum the differences between consecutive pairs in the array in descending order.
 
+function sumOfDifferences(arr) {
+    let sum = 0
+    arr = arr.sort((a, b) => b - a);
+    for (let i = 0; i < arr.length - 1; i++) {
+        sum += arr[i] - arr[i + 1]
+    }
+    return sum
+}
+
