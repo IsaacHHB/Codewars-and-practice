b4c8869abda1f43f6d074d53ec8cd2d34c29f173
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-23 13:46:05 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -228,3 +228,7 @@ function sumOfDifferences(arr) {
 //No Loops 2 - You only need one
 //You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
 
+function check(a, x) {
+    return a.includes(x);
+};
+
