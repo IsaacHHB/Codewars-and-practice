610841cf79af920eca99554f3e5e36fb8b3eafb5
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-24 13:15:44 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -265,3 +265,15 @@ function remove(s, n) {
 // Sock Merchant
 // There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
+function sockMerchant(n, ar) {
+    let socks = {};
+    let pairs = 0;
+    for (let i of ar) {
+        socks[i] = socks[i] + 1 || 1;
+        if (socks[i] % 2 === 0) {
+            pairs += 1;
+        }
+    }
+    return pairs;
+}
+
