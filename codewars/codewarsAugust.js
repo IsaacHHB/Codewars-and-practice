cf26e9776e50c8f0aa7bde19bba68cd4b4cffc38
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-25 17:59:59 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -290,3 +290,9 @@ elevator = (left, right, call) => Math.abs(call - left) < Math.abs(call - right)
 //Simple validation of a username with regex
 //Write a simple regex to validate a username. Allowed characters are: lowercase letters, numbers, underscore Length should be between 4 and 16 characters (both included).
 
+function validateUsr(username) {
+    return username.length >= 4
+        && username.length <= 16
+        && (username.match(/[a-z]|_|\d/g).length === username.length);
+}
+
