565002af0c2b7f75b9b8b84bb5a6571f3b5e96a7
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-26 14:27:14 -0700

    Update groupNight.js

---
+++ codewars/groupNight.js
@@ -0,0 +1,20 @@
+
+//two args that return an array first n multiples of x
+//assume both numbers are positive and greater than 0
+//return results as an array
+
+
+//P: x,n given as ints
+//R: return both ints as an array
+//E: [2,4,6,8,10]
+//P:
+
+function countBy(x,n){
+    let arr = []
+    //take x and multiply by n
+    for(let i = 1; i <= n; i++){
+        //push to an array
+        arr.push(x * i)
+    }
+    return arr;
+}
