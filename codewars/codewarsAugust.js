d53c767a6fee35179f6d81295768ec0551d03c7b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-26 14:21:46 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -361,3 +361,5 @@ whatday = (num) => num == 1 ? 'Sunday' : num == 2 ? 'Monday' : num == 3 ? 'Tuesd
 sixToast = (num) => num > 6 || num < 6 ? Math.abs(num - 6) : 0;
 
 //Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string
+//Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
