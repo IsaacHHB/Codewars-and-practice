a8cb0330716ce95d2562f5a5150e78785bc8c3d6
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-28 16:29:40 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -379,3 +379,7 @@ function getNumberFromString(s) {
 //Grasshopper - Combine strings
 //Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
 
+function combineNames(name1, name2) {
+    return `${name1} ${name2}`
+}
+
