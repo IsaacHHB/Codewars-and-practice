4addbc9bac9b3a4c61d0ef3647569880a275e72d
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-29 16:30:18 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -386,3 +386,14 @@ function combineNames(name1, name2) {
 //Convert string to camel case
 //Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
 
+function toCamelCase(str) {
+    str = str.split('');
+    return str.map(function (el, i) {
+        if (el == '-' || el == '_') {
+            el = str[i + 1].toUpperCase();
+            str.splice(i + 1, 1);
+        }
+        return el;
+    }).join('');
+}
+
