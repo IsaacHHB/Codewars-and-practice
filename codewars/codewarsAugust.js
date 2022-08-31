c614ae18c7e288e857984403c7f4023b885e24b3
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-08-31 15:24:15 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -424,3 +424,19 @@ function sumStrings(a, b) {
 //Human readable duration format
 //Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way. The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds. It is much easier to understand with an example: * For seconds = 62, your function should return "1 minute and 2 seconds" * For seconds = 3662, your function should return "1 hour, 1 minute and 2 seconds"
 
+function formatDuration(seconds) {
+    var time = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 },
+        res = []
+
+    if (seconds === 0) return 'now';
+
+    for (var key in time) {
+        if (seconds >= time[key]) {
+            var val = Math.floor(seconds / time[key]);
+            res.push(val += val > 1 ? ' ' + key + 's' : ' ' + key);
+            seconds = seconds % time[key];
+        }
+    }
+    return res.length > 1 ? res.join(', ').replace(/,([^,]*)$/, ' and' + '$1') : res[0]
+}
+
