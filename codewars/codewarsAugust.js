2ce144d09f1af1066ea71208827fc61d0799c291
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-01 12:35:14 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -448,5 +448,7 @@ function duplicateCount(text) {
 }
 
 //Replace With Alphabet Position
+//In this kata you are required to, given a string, replace every letter with its position in the alphabet. If anything in the text isn't a letter, ignore it and don't return it.
+
 
 
