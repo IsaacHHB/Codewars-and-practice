122fdecbba927d83a1ec9e9a518425280bf1a832
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-02 12:57:09 -0700

    Update codewarsSep.js

--- codewarsSep.js
+++ codewarsSep.js
@@ -1,1 +1,3 @@
 //Find the capitals
+//Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
