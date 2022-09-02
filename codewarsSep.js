88195981c5e960369a2ab8630755ee0314993ccf
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-02 12:57:19 -0700

    Update codewarsSep.js

--- codewarsSep.js
+++ codewarsSep.js
@@ -1,3 +1,11 @@
 //Find the capitals
 //Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
 
+var capitals = function (word) {
+    var caps = [];
+    for (var i = 0; i < word.length; i++) {
+        if (word[i].toUpperCase() == word[i]) caps.push(i);
+    }
+    return caps;
+};
+
