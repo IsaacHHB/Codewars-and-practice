dabf09c89e4294928b59c71d2a2f53ffe54f5329
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-03 13:01:55 -0700

    Update codewarsSep.js

--- codewarsSep.js
+++ codewarsSep.js
@@ -62,3 +62,12 @@ array = arr => arr.split(",").slice(1, -1).join(" ") || null;
 //Training JS #10: loop statement --for
 //Coding in function pickIt, function accept 1 parameter:arr, it's a number array, we need traverse arr by using for loop, if element is odd number, push it to array odd, if it's a even number, push it to array even. I've defined two array odd and even in the function, and also wrote the return statement. your work is write a for loop.
 
+function pickIt(arr) {
+    var odd = [], even = [];
+    for (element of arr)
+        (element % 2 ? odd : even).push(element)
+
+
+    return [odd, even];
+}
+
