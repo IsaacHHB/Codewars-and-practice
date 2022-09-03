2fd619a5fa4b4f04c0f1b30cfb2f92ce1c65582c
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-03 13:01:31 -0700

    Update codewarsSep.js

--- codewarsSep.js
+++ codewarsSep.js
@@ -57,3 +57,5 @@ String.prototype.capitalize = function () {
 //Remove First and Last Character Part Two
 //You are given a string containing a sequence of character sequences separated by commas. Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
 
+array = arr => arr.split(",").slice(1, -1).join(" ") || null;
+
