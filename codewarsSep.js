e6ad2b12f916f7068bc1d91c8c272497305f840d
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-03 13:01:08 -0700

    Update codewarsSep.js

--- codewarsSep.js
+++ codewarsSep.js
@@ -46,3 +46,11 @@ function defineSuit(card) {
 //Write a function capitalize() which capitalizes the first letter (if any) of the given string.
 //JavaScript / CoffeeScript: Extend the String prototype with a method capitalize() so you can call it on a string like so: "string".capitalize(). Learn about inheritance and the prototype chain. Furthermore, the built-in string methods toUpperCase() and toLowerCase() are disabled for this Kata.
 
+String.prototype.capitalize = function () {
+    let code = this.charCodeAt(0)
+    if (code > 96 && code < 123) {
+        code -= 32
+    }
+    return String.fromCharCode(code) + this.slice(1)
+}
+
