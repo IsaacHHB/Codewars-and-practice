21d320ee7e5a3bb29e4d2c59da17d2336b9ea0d0
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-04 12:07:41 -0700

    Update codewarsSep.js

--- codewarsSep.js
+++ codewarsSep.js
@@ -74,3 +74,9 @@ function pickIt(arr) {
 //Remove anchor from URL
 //Complete the function/method so that it returns the url with anything after the anchor (#) removed.
 
+function removeUrlAnchor(url) {
+    const index = url.indexOf("#");
+
+    return index === -1 ? url : url.slice(0, index);
+}
+
