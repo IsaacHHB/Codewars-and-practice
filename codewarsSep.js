91550284a678bc4dc5cc6d33e2877c6fa6c30244
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-04 12:07:59 -0700

    Update codewarsSep.js

--- codewarsSep.js
+++ codewarsSep.js
@@ -83,3 +83,10 @@ function removeUrlAnchor(url) {
 //Is there a vowel in there?
 //Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u). If they are, change the array value to a string of that vowel. Return the resulting array.
 
+const isVow = a =>
+  a.map(
+    charCode =>
+      /[aeiou]/.test(String.fromCharCode(charCode))
+        ? String.fromCharCode(charCode)
+        : charCode
+  )
