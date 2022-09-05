c0ba2ee5fbca4effdb3415e745f56702069e036f
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-05 12:16:29 -0700

    Update codewarsSep.js

--- codewarsSep.js
+++ codewarsSep.js
@@ -99,3 +99,4 @@ function contamination(text, char) {
 }
 
 //Template Strings
+//Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings Your task is to return the correct string using the Template String Feature. Input Two Strings, no validation is needed. Output You must output a string containing the two strings with the word ```' are '```
