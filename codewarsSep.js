91422ff5dfa7298a3f5254e5ec64ee7f056584b8
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-05 12:13:47 -0700

    Update codewarsSep.js

--- codewarsSep.js
+++ codewarsSep.js
@@ -94,6 +94,6 @@ const isVow = a =>
 //Contamination #1 -String-
 //An AI has infected a text with a character!! This text is now fully mutated to this character. If the text or the character are empty, return an empty string. There will never be a case when both are empty as nothing is going on!! Note: The character is a string of length 1 or an empty string.
 
-function contamination(text, char){
+function contamination(text, char) {
     return char.repeat(text.length)
-  }
+}
