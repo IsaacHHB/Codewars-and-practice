697aa572b504f388afbb2b09967a3a2a41254c8d
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-08 13:34:07 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -503,12 +503,12 @@ duckDuckGoose = (players, goose) => players[(goose - 1) % players.length].name
 //pick a set of first elements
 //Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence. If n == 0 return an empty sequence []
 
-function first(arr, n =1 ) {
-    if (n == 0){
-    return [];
-    }else if(n>0){
-      return arr.slice(0, n);
-      }
+function first(arr, n = 1) {
+    if (n == 0) {
+        return [];
+    } else if (n > 0) {
+        return arr.slice(0, n);
     }
+}
 
 
