c38a128d6646a41abb32d5a1a772ce478f33fd70
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-17 14:09:58 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -557,3 +557,5 @@ function dontGiveMeFive(start, end) {
 }
 
 //Ones and Zeros
+//Given an array of ones and zeroes, convert the equivalent binary value to an integer. Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1. However, the arrays can have varying lengths, not just limited to 4.
+
