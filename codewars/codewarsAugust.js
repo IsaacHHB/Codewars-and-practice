b46219446185f09b3c4bd3793e026a829635b081
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-09-18 18:31:59 -0700

    Update codewarsAugust.js

--- codewars/codewarsAugust.js
+++ codewars/codewarsAugust.js
@@ -567,6 +567,6 @@ const binaryArrayToNumber = arr => {
 //Sort array by string length
 //Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
 
-function sortByLength (array) {
+function sortByLength(array) {
     return array.sort((a, b) => a.length - b.length);
-  };
+};
