e94429f31f13ca8b1f60fed92af5d0573a9cf938
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-10 18:49:58 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -27,15 +27,15 @@ function maskify(cc) {
 //FIXME: Get Full Name
 //The code provided is supposed return a person's Full Name given their first and last names. But it's not working properly. Notes The first and/or last names are never null, but may be empty. Task Fix the bug so we can all go home early.
 
-class Dinglemouse{
+class Dinglemouse {
 
-    constructor( firstName, lastName ){
-      this._firstName=firstName;
-      this._lastName=lastName;
+    constructor(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
     }
-    
-    getFullName(){
-      return `${this._firstName} ${this._lastName}`.trim();
+
+    getFullName() {
+        return `${this._firstName} ${this._lastName}`.trim();
     }
-    
-  }
+
+}
