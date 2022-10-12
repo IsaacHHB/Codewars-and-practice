ba2a58cdaf08c7795cc48b0c64f3fd8b5ec82c46
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-12 12:11:26 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -49,3 +49,6 @@ function add(a, b){
 
 //String ends with?
 //Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+solution = (str, ending) => str.endsWith(ending)
+
