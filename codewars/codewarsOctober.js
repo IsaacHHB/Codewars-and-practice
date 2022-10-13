fc8e4d074cb46b0767a2b1cc46d7f33eed06e892
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-13 15:16:33 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -85,3 +85,6 @@ var twoSum = function(nums, target) {
 //Extract the domain name from a URL
 //Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. 
 
+function domainName(url){
+    return url.replace('https://', '').replace('http://', '').replace('www.', '').split('.')[0];
+  }
