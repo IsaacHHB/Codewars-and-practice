59b2422678ed1078b858be62c16e44f9cd801403
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-13 15:16:16 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -72,3 +72,13 @@ function reverseWords(str) {
 //Two Sum
 //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
 
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(nums[i] + nums[j] === target && i !== j){
+                return [i,j]
+            }
+        }
+    }
+};
+
