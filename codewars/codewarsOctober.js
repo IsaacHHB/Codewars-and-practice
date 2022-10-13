28f20d26a367348491ddb71bacb47bf319d2de7e
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-13 15:15:59 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -64,3 +64,9 @@ function palindrome(a) {
 //Reverse words
 //Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
+function reverseWords(str) {
+    str = str.split(' ').reverse().join(' ')
+    return str.split('').reverse().join('')
+  }
+
+//Two Sum
