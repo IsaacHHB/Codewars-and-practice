11c80cec5b2c32d09372a506d9eb13625e487a75
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-17 16:21:55 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -120,3 +120,4 @@ const romanToInt = (s) => {
 };
 
 //Sum of Multiples
+//Find the sum of all multiples of n below m Keep in Mind n and m are natural numbers (positive integers) m is excluded from the multiples
