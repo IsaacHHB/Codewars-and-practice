631b1ecfdf9ef792f5d563debc3352ab9a07cf54
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-19 12:05:15 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -134,7 +134,11 @@ function sumMul(n, m) {
 //It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
 //Return the average of the given array rounded down to its nearest integer. The array will never be empty.
 
-
+function getAverage(arr) {
+    let leng = arr.length
+    arr = arr.reduce((a,c) => a + c, 0) / leng
+    return Math.floor(arr)
+}
 
 
 
