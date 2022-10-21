2d2b0bf73b19c47992e14e984cc99cd642cd3657
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-20 17:16:16 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -142,7 +142,7 @@ function getAverage(arr) {
 
 // console.log(getAverage([2, 2, 2, 2]), 2)
 // console.log(getAverage([1, 2, 3, 4, 5,]), 3);
-
+// console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2]), 1)
 
 
 
