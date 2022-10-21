c8e42f22853b34915ddb3420c49c255943d3aa53
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-21 16:56:42 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -189,6 +189,8 @@ function evenNumbers(array, num) {
     return arr.slice(-num)
 }
 
+//more effecient
+
 
 // ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
 // ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
