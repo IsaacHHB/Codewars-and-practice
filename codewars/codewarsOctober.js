67a17fad3e5cb1a003e98266bd404141e8a01dd7
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-23 13:34:57 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -202,3 +202,6 @@ function evenNumbers(array, number) {
 //Time to test your basic knowledge in functions! Return the odds from a list:
 
 const odds = values => values.filter(e => e % 2 !== 0 );
+
+//[1, 2, 3, 4, 5]  -->  [1, 3, 5]
+//[2, 4, 6]        -->  []
