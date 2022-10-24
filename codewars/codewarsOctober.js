945ca9cae03400d3d50a2c0c1e782a71563d40f8
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-24 15:04:07 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -192,7 +192,7 @@ function evenNumbers(array, num) {
 //more effecient solution
 function evenNumbers(array, number) {
     return array.filter(e => e % 2 === 0).slice(-number)
-  }
+}
 
 // ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
 // ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
@@ -201,7 +201,29 @@ function evenNumbers(array, number) {
 //Are arrow functions odd?
 //Time to test your basic knowledge in functions! Return the odds from a list:
 
-const odds = values => values.filter(e => e % 2 !== 0 );
+const odds = values => values.filter(e => e % 2 !== 0);
 
 //[1, 2, 3, 4, 5]  -->  [1, 3, 5]
 //[2, 4, 6]        -->  []
+
+//Remove the time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
