526c3684dc857d901c25620a537af4d10b9a9c96
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-24 15:04:48 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -224,7 +224,7 @@ function arrayDiff(a, b) {
 //arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
 //Friend or Foe?
-
+//Make a program that filters a list of strings and returns a list with only your friends name in it. If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not... Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
 
 
 
