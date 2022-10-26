08ab63de84e3449cef449cbef2ea89c0e4a292f0
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-26 15:14:36 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -268,7 +268,9 @@ function solve(arr) {
 
 //Given an array of integers , Find the Nth smallest element in this array of integers. //Notes Array/list size is at least 3 . Array/list's numbers could be a mixture of positives , negatives and zeros . Repetition in array/list's numbers could occur , so don't Remove Duplications .
 
-
+function nthSmallest(arr, pos){
+    return arr.sort((a,b) => a - b)[pos-1]
+  }
 
 
 
