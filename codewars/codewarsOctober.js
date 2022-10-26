b4c95818e7d65774af0c7d3e81adb5ee78b5e869
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-26 15:14:50 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -276,3 +276,4 @@ function nthSmallest(arr, pos){
 // console.log(nthSmallest([3,1,2] ,2)) //==> return (2) 
 // console.log(nthSmallest([15,20,7,10,4,3] ,3)) //==> return (7) 
 // console.log(nthSmallest([15,20,7,10,4,3] ,3))// ==> return (7) 
+// console.log(nthSmallest([177,225,243,-169,-12,-5,2,92] ,5)) //==> return (92) 
