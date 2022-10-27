61da480072b0db1bd916ae9025d5aa776be4c980
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-27 14:55:11 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -281,7 +281,10 @@ function nthSmallest(arr, pos){
 //Duplicate Encoder
 //The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
-
+function duplicateEncode(word){
+    let w = word.toLowerCase().split('')
+    return w.map(x => w.indexOf(x) == w.lastIndexOf(x)? '(' : ')').join('')
+  }
 
 
 
