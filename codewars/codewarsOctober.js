5bc939d576c9605761061e6e1c590d020c49aa5c
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-27 14:55:07 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -279,7 +279,7 @@ function nthSmallest(arr, pos){
 // console.log(nthSmallest([177,225,243,-169,-12,-5,2,92] ,5)) //==> return (92) 
 
 //Duplicate Encoder
-
+//The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
 
 
