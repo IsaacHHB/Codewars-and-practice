59d1949eb972acf50bcc71cd6044ba400f47ffec
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-28 15:15:35 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -310,3 +310,11 @@ function digitalRoot(n) {
 //Unique In Order
 //Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 
+function uniqueInOrder(iterable){
+    let strArr = Array.isArray(iterable) ? iterable : iterable.split('');
+    let unique = strArr.filter((letter, i) => {
+      return strArr[i] != strArr[i+1];
+    })
+  
+    return unique;
+  }
