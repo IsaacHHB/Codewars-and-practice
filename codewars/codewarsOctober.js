102eee6e4ea921cb75cfe3360df800cdc2b3118e
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-10-28 15:15:08 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -294,7 +294,13 @@ function duplicateEncode(word){
 //Sum of Digits / Digital Root
 //Digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
 
-
+function digitalRoot(n) {
+    let result = 0;
+    String(n).split('').map(num => {
+      result += Number(num);
+    });
+    return result >= 10 ? digitalRoot(result) : result;
+  }
 
 
 
