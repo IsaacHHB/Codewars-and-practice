3e82ca6ae8af4669d84088c0e7618c78795c9d5d
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-01 12:39:59 -0700

    Update codewarsOctober.js

--- codewars/codewarsOctober.js
+++ codewars/codewarsOctober.js
@@ -340,4 +340,17 @@ function DNAStrand(dna) {
 //Given a string, return a new string that has transformed based on the input: Change case of every character, ie. lower case to upper case, upper case to lower case. Reverse the order of words from the input. Note: You will have to handle multiple spaces, and leading/trailing spaces. You may assume the input only contain English alphabet and spaces.
 // "Example Input" ==> "iNPUT eXAMPLE"
 
+function reverseCapitals(str) {
+    let res = ''
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === str[i].toLowerCase()){
+            res += str[i].toUpperCase()
+        }else{
+            res += str[i].toLowerCase()
+        }
+    }
+    return res.split(' ').reverse().join(' ')
+
+}
+
 
