c90a6b0aebb3f3ad32cfa187441ce967848b043b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-02 12:58:22 -0700

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -3,6 +3,6 @@
 
 const last = x =>  x.split(' ').sort((a,b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1))
 
-
+console.log(last('man i need a taxi up to ubud'), "['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']");
 
   
