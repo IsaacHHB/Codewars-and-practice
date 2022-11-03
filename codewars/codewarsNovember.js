dd313556073c2729a60588d38f81cb84ed60ff90
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-03 12:00:51 -0700

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -6,3 +6,7 @@ const last = x => x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.cha
 //console.log(last('man i need a taxi up to ubud'), "['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']");
 //console.log(last('what time are we climbing up the volcano'), "['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']");
 //console.log(last('take me to semynak'), "['take', 'me', 'semynak', 'to']");   
+
+//Bit Counting
+
+
