544a72213a943282d29379daae306817c171400e
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-03 12:00:56 -0700

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -8,5 +8,5 @@ const last = x => x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.cha
 //console.log(last('take me to semynak'), "['take', 'me', 'semynak', 'to']");   
 
 //Bit Counting
-
+//Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative. Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 
