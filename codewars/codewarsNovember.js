63cabe5d69b3d762ca4d540c95dd911788e818d1
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-04 13:42:45 -0700

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -51,7 +51,7 @@ function isValidWalk(walk) {
     return walk.length == 10 && ns === 0 && we === 0;
 }
 
-console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']), 'true');
+//console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']), 'true');
 console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']), 'false');
 console.log(isValidWalk(['w']), 'false');
 console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']), 'false');
