55f169779081a1ce4a3e08413dde0bd0711dba1c
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-06 17:39:33 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -68,7 +68,7 @@ function findDeletedNumber(arr, mixArr) {
 
 // findDeletedNumber([1,2,3,4,5], [3,4,1,5]), 2, 'Deletion')
 // findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]), 5, 'Deletion')
-
+// findDeletedNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]), 0, 'No deletion')
 
 
 
