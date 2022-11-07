b203e4f84f85f8cf977226824ca82600497db9aa
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-06 17:39:46 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -73,7 +73,10 @@ function findDeletedNumber(arr, mixArr) {
 //Find the unique number
 //There is an array with some numbers. All numbers are equal except for one. Try to find it! It’s guaranteed that array contains at least 3 numbers. The tests contain some very huge arrays, so think about performance.
 
-
+function findUniq(arr) {
+    arr = arr.filter((l,i,a) => a.indexOf(l) === a.lastIndexOf(l)).join('')
+    return +arr
+  }
 
 
 
