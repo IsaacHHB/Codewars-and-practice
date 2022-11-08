007fdbe557b98d620b59cfa6244cbdf426028bfe
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-08 15:00:22 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -95,3 +95,7 @@ function isPangram(string) {
             return string.indexOf(x) !== -1;
         });
 }
+
+
+//isPangram("The quick brown fox jumps over the lazy dog."), true)
+isPangram("This is not a pangram."), false)
