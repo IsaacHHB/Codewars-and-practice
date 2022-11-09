a8f17ff69ae9fda29e30acd5b00128575fc9b4fd
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-09 12:06:31 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -100,3 +100,4 @@ function isPangram(string) {
 //isPangram("This is not a pangram."), false)
 
 //Persistent Bugger.
+//Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
