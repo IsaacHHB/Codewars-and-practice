80de28153e4ad76a0ef53669b9b54f465e124505
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-11 15:31:55 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -132,6 +132,6 @@ function arrayDiff(a, b) {
 //You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
 function sortArray(array) {
-    const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+    const odd = array.filter((x) => x % 2).sort((a, b) => a - b);
     return array.map((x) => x % 2 ? odd.shift() : x);
-  }
+}
