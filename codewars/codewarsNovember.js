8412527f5813ffcd5dd0fa121e9a08b35cd7254f
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-11 15:31:27 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -127,3 +127,5 @@ function arrayDiff(a, b) {
 // arrayDiff([], [4,5]), [], "a was [], b was [4,5]"
 // arrayDiff([3,4], [3]), [4], "a was [3,4], b was [3]"
 // arrayDiff([1,8,2], []), [1,8,2], "a was [1,8,2], b was []"
+
+//Sort the odd
