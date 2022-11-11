1f3428538acd36c92a9cbb3918fa11dc24dee5ab
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-10 16:20:03 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -118,7 +118,9 @@ function persistence(n) {
 
 //Implement a difference function, which subtracts one list from another and returns the result. It should remove all values from list a, which are present in list b keeping their order. arrayDiff([1,2],[1]) == [2] If a value is present in b, all of its occurrences must be removed from the other:
 
-
+function arrayDiff(a, b) {
+    return a.filter(e => !b.includes(e))
+  }
 
 
 
