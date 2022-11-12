d9db5e0de85c25650107a02d4356d3ee5259db30
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-12 12:37:24 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -143,7 +143,7 @@ function sortArray(array) {
 //Find the middle element
 //As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.The input to the function will be an array of three distinct numbers (Haskell: a tuple).
 
-function gimme (triplet) {
-    let mid = [...triplet].sort((a,b) => a - b)[1]
+function gimme(triplet) {
+    let mid = [...triplet].sort((a, b) => a - b)[1]
     return triplet.indexOf(mid)
-  }
+}
