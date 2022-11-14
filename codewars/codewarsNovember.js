419a818c1d9e2d0f2c9565f18aac5eb3df9272d3
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-14 12:40:19 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -151,7 +151,7 @@ function gimme(triplet) {
 
 //console.log(doTest([2, 3, 1]), 0);
 //console.log(doTest([5, 10, 14]), 1);
-console.log(doTest([2.1, 3.2, 1.4]), 0);
+//console.log(doTest([2.1, 3.2, 1.4]), 0);
 console.log(doTest([5.9, 10.4, 14.2]), 1);
 console.log(doTest([-2, -3, -1], 0));
 console.log(doTest([-5, -10, -14]), 1);
