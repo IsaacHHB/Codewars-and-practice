52a3a4027d37a4624ba62e873970d0e31bda7646
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-21 12:43:26 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -177,3 +177,5 @@ function averageLength(arr) {
 //Your task is to remove all consecutive duplicate words from a string, leaving only first words entries.
 
 const removeConsecutiveDuplicates = s => s.split(' ').filter((w, i, a) => w !== a[i + 1]).join(' ');
+
+removeConsecutiveDuplicates('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'), 'alpha beta gamma delta alpha beta gamma delta');
