8d12d67b4ce216722549a462f53ae695b2b05da6
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-24 22:21:26 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -204,3 +204,13 @@ function solution(string) {
 //Build Tower
 //Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
 
+function towerBuilder(nFloors) {
+    var tower = [];
+    for (var i = 0; i < nFloors; i++) {
+      tower.push(" ".repeat(nFloors - i - 1)
+      + "*".repeat((i * 2)+ 1)
+      + " ".repeat(nFloors - i - 1))
+    }
+  return tower;
+}
+
