8789ef434cececda2ec18d50e6282a0abe53c190
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-25 16:18:50 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -219,3 +219,5 @@ function towerBuilder(nFloors) {
 // console.log(towerBuilder(3), ["  *  "," *** ","*****"]);
 
 //Stop gninnipS My sdroW!
+//Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
