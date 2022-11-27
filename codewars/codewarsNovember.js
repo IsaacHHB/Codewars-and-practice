86eff29d18e042c44b37a99ccbd23f75cd547e52
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-11-26 16:19:51 -0800

    Update codewarsNovember.js

--- codewars/codewarsNovember.js
+++ codewars/codewarsNovember.js
@@ -231,3 +231,4 @@ function spinWords(string) {
 //   console.log(spinWords("Hey fellow warriors"), "Hey wollef sroirraw");
 //   console.log(spinWords("This is a test"), "This is a test");
 //   console.log(spinWords("This is another test"), "This is rehtona test");
+//   console.log(spinWords("You are almost to the last test"), "You are tsomla to the last test");
