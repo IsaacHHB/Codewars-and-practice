bd46a84fcdfa0acfa7cbba69bddafa972e3f472b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-03 12:32:33 -0800

    Update codewarsDec.js

--- codewars/codewarsDec.js
+++ codewars/codewarsDec.js
@@ -22,3 +22,4 @@ var cubeChecker = function (volume, side) {
 // console.log(cubeChecker(-8,-2), false);
 // console.log(cubeChecker(0, 0), false);
 // console.log(cubeChecker(1, 5),  false);
+// console.log(cubeChecker(125, 5), true);
