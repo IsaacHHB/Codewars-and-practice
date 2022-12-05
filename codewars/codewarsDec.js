8ac7d87e745be1fe1c1984e71ec3a91f50753d6e
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-05 15:03:43 -0800

    Update codewarsDec.js

--- codewars/codewarsDec.js
+++ codewars/codewarsDec.js
@@ -35,3 +35,4 @@ const smash = words => words.join(' ')
 //console.log(smash(["this", "is", "a", "really", "long", "sentence"]), "this is a really long sentence");
 
 //Count characters in your string
+//The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}. What if the string is empty? Then the result should be empty object literal, {}.
