b706e739ddcedfc09487c081afe0b573346c2341
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-05 15:13:42 -0800

    Update codewarsDec.js

--- codewars/codewarsDec.js
+++ codewars/codewarsDec.js
@@ -37,19 +37,19 @@ const smash = words => words.join(' ')
 //Count characters in your string
 //The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}. What if the string is empty? Then the result should be empty object literal, {}.
 
-function count (string) {  
+function count(string) {
     //create obj
     let countObj = {}
     //loop -> check val exist ? val++ : val-1
-    for(const c of string){
-      if(countObj[c]){
-        countObj[c]++
-      }else{
-        countObj[c] = 1
-      }
+    for (const c of string) {
+        if (countObj[c]) {
+            countObj[c]++
+        } else {
+            countObj[c] = 1
+        }
     }
     return countObj
-  }
+}
 
-// console.log(count("aba"), { a: 2, b: 1 }); 
+// console.log(count("aba"), { a: 2, b: 1 });
 // console.log(count(""), {});   
