552a0c433d881bee8b4694485e9f83ae9b501894
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-07 14:28:53 -0800

    Update codewarsDec.js

--- codewars/codewarsDec.js
+++ codewars/codewarsDec.js
@@ -69,3 +69,7 @@ function openOrSenior(data) {
 //?console.log(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]),['Open', 'Open', 'Open', 'Open'])
 //?console.log(openOrSenior([[59, 12],[55,-1],[12, -2],[12, 12]]),['Senior', 'Open', 'Open', 'Open'])
 
+//+ Find the stray number
+
+ 
+
