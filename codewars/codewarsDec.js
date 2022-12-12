c18b8a96e49e4532d997615d9203edc778ca02b9
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-12 09:33:42 -0800

    Update codewarsDec.js

--- codewars/codewarsDec.js
+++ codewars/codewarsDec.js
@@ -138,3 +138,5 @@ console.log(isDigit("s2324"), false);
 console.log(isDigit("-234.4"), true); 
 
 //+ Split Strings
+//+ Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
