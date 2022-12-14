61c1dedb5fc76be2e03504426d4aed41c9d606ff
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-14 15:24:59 -0800

    Update codewarsDec.js

--- codewars/codewarsDec.js
+++ codewars/codewarsDec.js
@@ -173,9 +173,9 @@ function containsDuplicate(array) {
     return false
 }
 
-console.log(containsDuplicate([1,2,3,1]), true)
-console.log(containsDuplicate([1,2,3,4]), false)
-console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]), true)
-console.log(containsDuplicate([2,14,18,22,22]), true)
+//? console.log(containsDuplicate([1,2,3,1]), true)
+ console.log(containsDuplicate([1,2,3,4]), false)
+ console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]), true)
+ console.log(containsDuplicate([2,14,18,22,22]), true)
 
 
