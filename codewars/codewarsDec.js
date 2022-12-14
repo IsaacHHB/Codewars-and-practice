479307204bd82d972e35f74b89a6710846c6b71b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-14 15:23:52 -0800

    Update codewarsDec.js

--- codewars/codewarsDec.js
+++ codewars/codewarsDec.js
@@ -157,4 +157,6 @@ function solution(str) {
 //? console.log(solution(""), []);
 
 //+ Contains Duplicate
+//+ Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
 
