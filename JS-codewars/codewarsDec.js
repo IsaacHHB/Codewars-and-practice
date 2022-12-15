265ab8a6a4f6648ac8dbd5af0b36161a15e880d2
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-15 11:19:55 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -179,4 +179,6 @@ function containsDuplicate(array) {
 //? console.log(containsDuplicate([2,14,18,22,22]), true)
 
 //+ Majority Element
+//+ Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
 
