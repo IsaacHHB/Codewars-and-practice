e1cf8e644a079b2d6b1cc4fcc55e467ff64e5838
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-16 08:02:05 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -232,6 +232,6 @@ function ransomNote(note, magazine) {
 //? console.log(ransomNote("sit ad est sint", magazine), true);
 //? console.log(ransomNote("sit ad est love", magazine), false);
 //? console.log(ransomNote("sit ad est sint in in", magazine), true);
-console.log(ransomNote("sit ad est sint in in in in", magazine), false);
+//? console.log(ransomNote("sit ad est sint in in in in", magazine), false);
 
 
