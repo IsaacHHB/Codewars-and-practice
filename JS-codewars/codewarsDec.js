4a8d7ad4127273b97133664e7f75fff4f17ad2ac
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-18 12:32:33 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -272,4 +272,4 @@ function countSmileys(arr) {
 
 //? console.log(countSmileys([]                             ), 0);
 //? console.log(countSmileys([':D',':~)',';~D',':)']        ), 4);
-console.log(countSmileys([':)',':(',':D',':O',':;']     ), 2);
+//? console.log(countSmileys([':)',':(',':D',':O',':;']     ), 2);
