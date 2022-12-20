7ea442dcb87232695bfb1086c2b9d6c6dc70c209
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-19 17:09:34 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -281,17 +281,17 @@ function countSmileys(arr) {
 const findMissingLetter = (array) => {
     // we can skip the 1st letter
     for (let i = 1; i < array.length; i++) {
-      // get the char code of the previous letter
-      const prev = array[i - 1].charCodeAt();
-      // get the char code of the current letter
-      const current = array[i].charCodeAt();
-      
-      if (current - prev !== 1) { // if the difference between current and previous is not 1
-        // get the character after the previous 
-        return String.fromCharCode(prev + 1);
-      }
+        // get the char code of the previous letter
+        const prev = array[i - 1].charCodeAt();
+        // get the char code of the current letter
+        const current = array[i].charCodeAt();
+
+        if (current - prev !== 1) { // if the difference between current and previous is not 1
+            // get the character after the previous 
+            return String.fromCharCode(prev + 1);
+        }
     }
-    
+
     return null; // if nothing is found
-  }
+}
 
