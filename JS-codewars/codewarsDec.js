b3ffabc3fd27f331792de65ebe43d6ea9b6a6971
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-19 17:09:29 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -278,3 +278,20 @@ function countSmileys(arr) {
 //+ Find the missing letter
 //+ Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array. You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2. The array will always contain letters in only one case.
 
+const findMissingLetter = (array) => {
+    // we can skip the 1st letter
+    for (let i = 1; i < array.length; i++) {
+      // get the char code of the previous letter
+      const prev = array[i - 1].charCodeAt();
+      // get the char code of the current letter
+      const current = array[i].charCodeAt();
+      
+      if (current - prev !== 1) { // if the difference between current and previous is not 1
+        // get the character after the previous 
+        return String.fromCharCode(prev + 1);
+      }
+    }
+    
+    return null; // if nothing is found
+  }
+
