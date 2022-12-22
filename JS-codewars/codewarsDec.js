47f25c775ba94cbed6bb30654ec16bc6d8bba02d
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-21 17:11:36 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -305,12 +305,12 @@ const findMissingLetter = (array) => {
 
 function isPrime(num) {
     if (num < 2) {
-       return false;
+        return false;
     }
-  for (let i = 2; i < (num / 2) + 1; i++) {
-       if (num % i === 0) {
-         return false;
-       }
+    for (let i = 2; i < (num / 2) + 1; i++) {
+        if (num % i === 0) {
+            return false;
+        }
     }
-  return true;
+    return true;
 }
