6df3fa73d80c0ed3f061501ac399b867aeaae4d4
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-21 17:11:22 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -303,3 +303,14 @@ const findMissingLetter = (array) => {
 //+ Is a number prime?
 //+ Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime. Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
 
+function isPrime(num) {
+    if (num < 2) {
+       return false;
+    }
+  for (let i = 2; i < (num / 2) + 1; i++) {
+       if (num % i === 0) {
+         return false;
+       }
+    }
+  return true;
+
