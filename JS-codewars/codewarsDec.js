983b6acbbe3e8d41ac2256f898bd237ef7b8ba15
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-24 15:09:57 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -340,3 +340,5 @@ function queueTime(customers, n) {
 //? console.log(queueTime([2,3,10,2], 2), 12);
 
 //+ Find the divisors!
+//+ Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
