1d1755a1082939330acab6b338ced7fa23daf9e1
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-25 14:05:13 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -354,7 +354,7 @@ function divisors(integer) {
 
 //? console.log(divisors(14),        [2, 7]);
 //? console.log(divisors(15),        [3, 5]);
-console.log(divisors(12),  [2, 3, 4, 6]);
+//? console.log(divisors(12),  [2, 3, 4, 6]);
 console.log(divisors(20), [2, 4, 5, 10]);
 console.log(divisors(13), "13 is prime");
 console.log(divisors(97), "97 is prime");
