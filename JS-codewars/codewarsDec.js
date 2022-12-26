0623d8fc87c37269448c2c808939b8227010dcec
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-26 14:06:27 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -361,3 +361,16 @@ function divisors(integer) {
 
 //+ Find The Parity Outlier
 //+ You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+function findOutlier(integers){
+    let oddArr = [],
+        evenArr = []
+    for(let i = 0; i < integers.length; i++){
+      if(integers[i] % 2 === 0){
+        evenArr.push(integers[i])
+      }else{
+        oddArr.push(integers[i])
+      }
+    }
+    return oddArr.length < evenArr.length ? +oddArr : +evenArr
+  }
