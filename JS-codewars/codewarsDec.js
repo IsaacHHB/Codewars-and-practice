e2d6d62efe0a9b1be6fa544ba0e4ef5e4f6fefb1
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-26 14:08:49 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -375,6 +375,13 @@ function findOutlier(integers) {
     return oddArr.length < evenArr.length ? +oddArr : +evenArr
 }
 
+
+function findOutlier(integers){
+    const even = integers.filter(int => int % 2 === 0);
+    const odd  = integers.filter(int => int % 2 !== 0);
+    return even.length === 1 ? even[0] : odd[0];
+  }
+
 console.log(findOutlier([0, 1, 2]), 1)
 console.log(findOutlier([1, 2, 3]), 2)
 console.log(findOutlier([2,6,8,10,3]), 3)
