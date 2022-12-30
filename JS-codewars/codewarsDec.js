42e30d6950a5d1faf503dd8a66bf9ea7152df0f3
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-29 23:26:15 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -397,7 +397,7 @@ function high(s) {
     return s.split(' ')[as.indexOf(Math.max(...as))];
 }
 
-console.log(high('man i need a taxi up to ubud'),                'taxi');
+//? console.log(high('man i need a taxi up to ubud'),                'taxi');
 console.log(high('what time are we climbing up the volcano'), 'volcano'); 
 console.log(high('take me to semynak'),                       'semynak');   
 console.log(high('aa b'),                                          'aa');
