614d4ea860564085186e7621566fdf38ef80c6ca
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2022-12-30 23:27:47 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -421,3 +421,4 @@ function twoSum(numbers, target) {
 
 console.log(twoSum([1,2,3],          4),     [0,2])
 console.log(twoSum([1234,5678,9012], 14690), [1,2])
+console.log(twoSum([2,2,3],          4),     [0,1])
