299f3ad6ca8a4d663acd24407cdee4fc9b491eaa
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-02 18:28:29 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -444,3 +444,18 @@ function firstNonRepeatingLetter(s) {
 //+ Roman Numerals Encoder
 
 //+ Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer. Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+const solution = (num) => {
+    const roman = {
+      M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90,
+      L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1,
+    };
+    
+    let str = "";
+    for (var i of Object.keys(roman)) {
+      var q = Math.floor(num / roman[i]);
+      num -= q * roman[i];
+      str += i.repeat(q);
+    }
+    return str;
+  };
