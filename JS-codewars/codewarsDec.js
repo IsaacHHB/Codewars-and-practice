70d0851a180599067e2c872a86f5745475819f52
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-03 12:28:13 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -467,3 +467,5 @@ const solution = (num) => {
 //? console.log(solution(2008), 'MMVIII', '2008 should, "MMVIII"')
 
 //+ Who ate the cookie?
+//+ For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!" Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string) Note: Make sure you return the correct message with correct spaces and punctuation.
+
