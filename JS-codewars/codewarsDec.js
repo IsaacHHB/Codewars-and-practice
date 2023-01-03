94dac521b8266d24f9e843fb89d1dee9e0cb44ba
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-02 18:27:53 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -440,3 +440,5 @@ function firstNonRepeatingLetter(s) {
 //? console.log(firstNonRepeatingLetter('a'),       'a');
 //? console.log(firstNonRepeatingLetter('stress'),  't');
 //? console.log(firstNonRepeatingLetter('moonmen'), 'e');
+
+//+ Roman Numerals Encoder
