224fb234f5db969e9784093dcb9cd91e35974a6a
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-06 12:57:54 -0800

    Update codewarsDec.js

--- JS-codewars/codewarsDec.js
+++ JS-codewars/codewarsDec.js
@@ -479,7 +479,7 @@ function cookie(x) {
     }
 }
 
-console.log(cookie("Ryan"), "Who ate the last cookie? It was Zach!")
+//? console.log(cookie("Ryan"), "Who ate the last cookie? It was Zach!")
 console.log(cookie(26), "Who ate the last cookie? It was Monica!")
 console.log(cookie(2.3), "Who ate the last cookie? It was Monica!")
 console.log(cookie(true), "Who ate the last cookie? It was the dog!")
