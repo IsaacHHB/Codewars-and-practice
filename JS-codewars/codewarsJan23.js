b13442da913dd45580d70ed028dd937293c936fe
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-09 13:04:10 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -10,3 +10,10 @@ for (let i = 0; i < 1000; i++) {
 
 //+ Return to Sanity
 //+ This function should return an object, but it's not doing what's intended. What's wrong?
+
+function mystery() {
+    var results = {
+      sanity: 'Hello'
+    };
+    return results;
+  }
