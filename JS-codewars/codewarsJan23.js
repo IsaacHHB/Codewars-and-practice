8ee5b9dae51b0cc3f76cbee299d43ebddb73cf93
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-10 11:51:40 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -31,4 +31,5 @@ function findNextSquare(sq) {
 console.log(findNextSquare(121), 144);
 console.log(findNextSquare(625), 676);
 console.log(findNextSquare(319225), 320356);
+console.log(findNextSquare(15241383936), 15241630849);
 
