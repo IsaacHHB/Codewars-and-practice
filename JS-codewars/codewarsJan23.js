8bc2c223504b13cdccad738e2c7f9ef2cad25856
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-11 14:18:41 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -36,4 +36,4 @@ function findNextSquare(sq) {
 //? console.log(findNextSquare(342786627), -1);
 
 //+ Tribonacci Sequence
-//+ 
+//+ Well met with Fibonacci bigger brother, AKA Tribonacci. As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it
