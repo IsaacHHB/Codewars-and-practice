2e0555c27c0928b23c8038427c5ce31980d0c3be
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-12 13:19:50 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -49,7 +49,7 @@ function tribonacci(signature, n) {
 //? console.log(tribonacci([0,0,1], 10), [0,0,1,1,2,4,7,13,24,44]     )
 //? console.log(tribonacci([0,1,1], 10), [0,1,1,2,4,7,13,24,44,81]    )
 //? console.log(tribonacci([1,0,0], 10), [1,0,0,1,1,2,4,7,13,24]      )
-console.log(tribonacci([0,0,0], 10), [0,0,0,0,0,0,0,0,0,0]        )
+//? console.log(tribonacci([0,0,0], 10), [0,0,0,0,0,0,0,0,0,0]        )
 console.log(tribonacci([1,2,3], 10), [1,2,3,6,11,20,37,68,125,230])
 console.log(tribonacci([3,2,1], 10), [3,2,1,6,9,16,31,56,103,190] )
 console.log(tribonacci([1,1,1],  1), [1])
