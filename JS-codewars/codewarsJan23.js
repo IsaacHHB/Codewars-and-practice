bd0d31cb8baf65de20bd4aad61947c21443a89de
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-15 17:02:07 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -110,6 +110,6 @@ function findEvenIndex(arr) {
 
 //? console.log(findEvenIndex([1,2,3,4,3,2,1]), 3);
 //? console.log(findEvenIndex([1,100,50,-51,1,1]), 1);
-console.log(findEvenIndex([1,2,3,4,5,6]), -1);
+//? console.log(findEvenIndex([1,2,3,4,5,6]), -1);
 console.log(findEvenIndex([20,10,30,10,10,15,35]), 3);
 
