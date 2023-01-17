fa6d127f8ee0f1fa0607b905c3c8ea77521228b5
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-16 22:53:51 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -129,7 +129,7 @@ function wave(str) {
     return waveArr;
 }
 
-console.log(wave("codewars"), ["Codewars", "cOdewars", "coDewars", "codEwars", "codeWars", "codewArs", "codewaRs", "codewarS"]);
+//? console.log(wave("codewars"), ["Codewars", "cOdewars", "coDewars", "codEwars", "codeWars", "codewArs", "codewaRs", "codewarS"]);
 console.log(wave(""), []);
 console.log(wave("two words"), ["Two words", "tWo words", "twO words", "two Words", "two wOrds", "two woRds", "two worDs", "two wordS"]);
 console.log(wave(" gap "), [" Gap ", " gAp ", " gaP "])
