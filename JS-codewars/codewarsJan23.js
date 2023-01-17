95ca5ded0c6befc50a34edf10bcdf03eec0f3856
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-16 22:49:38 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -116,15 +116,15 @@ function findEvenIndex(arr) {
 //+ Mexican Wave
 //+ In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
 
-function wave(str){
+function wave(str) {
     let waveArr = [];
-    for(let i = 0; i < str.length; i++) {
-      let letter = str[i];
-      if (letter === " ") {
-        continue;
-      } else {
-        waveArr.push(str.slice(0, i) + letter.toUpperCase() + str.slice(i + 1))
-      } 
+    for (let i = 0; i < str.length; i++) {
+        let letter = str[i];
+        if (letter === " ") {
+            continue;
+        } else {
+            waveArr.push(str.slice(0, i) + letter.toUpperCase() + str.slice(i + 1))
+        }
     }
     return waveArr;
-  }
+}
