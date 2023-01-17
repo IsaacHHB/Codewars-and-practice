ab7cfa1b6b8b44b49d37f8d73466dbe68c195e9b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-17 13:02:53 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -135,3 +135,7 @@ function wave(str) {
 
 //+ Find Nearest square number
 //+ Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+function nearestSq(n){
+    return Math.pow(Math.round(Math.sqrt(n)), 2);
+  }
