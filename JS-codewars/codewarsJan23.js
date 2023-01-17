f11a5e60d7a874631aae8cbba3a68b7e975e9bba
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-17 13:02:45 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -134,3 +134,4 @@ function wave(str) {
 //? console.log(wave(" gap "), [" Gap ", " gAp ", " gaP "])
 
 //+ Find Nearest square number
+//+ Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
