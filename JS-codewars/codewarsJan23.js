d5ac6e7c56185b86b4c78d02b4ee466f88ee260b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-18 13:04:17 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -142,6 +142,6 @@ function nearestSq(n) {
 
 //? console.log(nearestSq(1),1)
 //? console.log(nearestSq(2),1)
-console.log(nearestSq(10),9)
+//? console.log(nearestSq(10),9)
 console.log(nearestSq(111),121)
 console.log(nearestSq(9999),10000)
