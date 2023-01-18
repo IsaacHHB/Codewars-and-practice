dc3f5f2f00a84d2dec5abd55d4931c3d5612933c
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-18 13:04:06 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -140,7 +140,7 @@ function nearestSq(n) {
     return Math.pow(Math.round(Math.sqrt(n)), 2);
 }
 
-console.log(nearestSq(1),1)
+//? console.log(nearestSq(1),1)
 console.log(nearestSq(2),1)
 console.log(nearestSq(10),9)
 console.log(nearestSq(111),121)
