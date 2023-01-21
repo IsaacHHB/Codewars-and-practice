2459c3747dbce9e7eb41caf3c3e09029110db5d7
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-20 16:09:50 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -166,13 +166,13 @@ const factorial = n => {
 //+ You will be given a number and you will need to return it as a string in Expanded Form. For example: expandedForm(12); // Should return '10 + 2' expandedForm(42); // Should return '40 + 2' expandedForm(70304); // Should return '70000 + 300 + 4' NOTE: All numbers will be whole numbers greater than 0.
 
 function expandedForm(num) {
-	num = String(num);
-	let result = [];
-	for (var i = 0; i < num.length; i++) {
-		if (num[i] == 0) continue;
-		else result.push(num[i] + "0".repeat(num.length -i -1))
-	}
-	return result.join(" + ")
+    num = String(num);
+    let result = [];
+    for (var i = 0; i < num.length; i++) {
+        if (num[i] == 0) continue;
+        else result.push(num[i] + "0".repeat(num.length - i - 1))
+    }
+    return result.join(" + ")
 }
 
 //? console.log(expandedForm(12), '10 + 2');
@@ -185,18 +185,18 @@ function expandedForm(num) {
 function longestConsec(strarr, k) {
     let arrLength = strarr.length;
     let arr = [];
-    
-    if(arrLength == 0 || k > arrLength || k <= 0) return "";
-      
-    for(let i = 0; i <= arrLength - k; i++){
-      let current = strarr[i];
-      for(let ii = k, jj = 1; ii > 1; ii--, jj++){
-        current += strarr[i+jj]
-      }
-    arr.push(current);
+
+    if (arrLength == 0 || k > arrLength || k <= 0) return "";
+
+    for (let i = 0; i <= arrLength - k; i++) {
+        let current = strarr[i];
+        for (let ii = k, jj = 1; ii > 1; ii--, jj++) {
+            current += strarr[i + jj]
+        }
+        arr.push(current);
     }
-   return arr.reduce(function (a, b) {
-          return a.length > b.length ? a : a.length == b.length ? a : b;
-      });
-  }
+    return arr.reduce(function (a, b) {
+        return a.length > b.length ? a : a.length == b.length ? a : b;
+    });
+}
 
