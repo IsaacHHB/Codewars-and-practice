b2eb63b79bdc8a9266bdda13515510bbd0af1b9d
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-20 16:09:37 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -180,3 +180,5 @@ function expandedForm(num) {
 //? console.log(expandedForm(70304), '70000 + 300 + 4');
 
 //+ Consecutive strings
+//+ You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
