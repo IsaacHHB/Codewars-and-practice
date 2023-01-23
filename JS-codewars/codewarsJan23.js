f3ab517f4d9cb1d6b616698dd3cea5728600dd0b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-23 15:52:29 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -179,6 +179,6 @@ function expandedForm(num) {
     return arr.join(" + ");
 }
 
-console.log(expandedForm(12), '10 + 2');
+//? console.log(expandedForm(12), '10 + 2');
 console.log(expandedForm(42), '40 + 2');
 console.log(expandedForm(70304), '70000 + 300 + 4');
