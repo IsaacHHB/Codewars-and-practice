6fd8ede68d7e63bf90d19c16b232b1e1b3f4108b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-22 16:11:53 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -207,5 +207,5 @@ function longestConsec(strarr, k) {
 //? console.log(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu")
 //? console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "")
 //? console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz")
-console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "")
+//? console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "")
 console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "")
