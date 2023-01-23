4f471b3f8fb9020ef8887aefaaf66874914aa25f
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-22 16:11:38 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -204,7 +204,7 @@ function longestConsec(strarr, k) {
 //? console.log(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh")
 //? console.log(longestConsec([], 3), "")
 //? console.log(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck")
-console.log(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu")
+//? console.log(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu")
 console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "")
 console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz")
 console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "")
