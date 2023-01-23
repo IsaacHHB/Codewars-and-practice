bcd5ecd1865ae9711cff7f1594f37c33ff6027a9
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-23 15:51:41 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -164,3 +164,17 @@ const factorial = n => {
 
 //+ Write Number in Expanded Form
 //+ You will be given a number and you will need to return it as a string in Expanded Form. For example: expandedForm(12); // Should return '10 + 2' expandedForm(42); // Should return '40 + 2' expandedForm(70304); // Should return '70000 + 300 + 4' NOTE: All numbers will be whole numbers greater than 0.
+
+function expandedForm(num) {
+    let arr = [],
+        multiple = 10
+    while(num > 1){
+        var remainder = num % multiple;
+      if(remainder >0){
+        arr.unshift(remainder);
+      }
+      num -= remainder;
+      multiple = multiple * 10;
+      }
+    return arr.join(" + ");
+  }
