b574046170cedfd213c6b889f67df7fa28782961
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-25 12:56:42 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -226,6 +226,6 @@ function toCsvText(array) {
 
 const numberToPower = (number, power) => power > 0 ? number * numberToPower(number, power - 1) : 1;
 
-console.log(numberToPower(4, 2), 16);
+//? console.log(numberToPower(4, 2), 16);
 console.log(numberToPower(10, 4), 10000);
 console.log(numberToPower(10, 0), 1);
