b2916fcea0141c283a2c5efa003c71d897a5f5c5
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-26 12:05:06 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -243,7 +243,7 @@ function getDivisorsCnt(n) {
     return output
 }
 
-console.log(getDivisorsCnt(1),  1);
+//? console.log(getDivisorsCnt(1),  1);
 console.log(getDivisorsCnt(10), 4);
 console.log(getDivisorsCnt(11), 2);
 console.log(getDivisorsCnt(54), 8);
