fc169001f51f1a5dcf71afa63942b4df21f271b4
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-26 12:04:08 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -232,3 +232,13 @@ const numberToPower = (number, power) => power > 0 ? number * numberToPower(numb
 
 //+ Count the divisors of a number
 //+ Count the number of divisors of a positive integer n.
+
+function getDivisorsCnt(n){
+    let output = 0;
+  for(let i  = 1; i <= n; i++){
+    if(n % i == 0){
+      output += 1;
+    }
+  }
+  return output
+}
