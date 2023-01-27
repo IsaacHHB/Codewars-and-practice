fcc656d011d18e193cdbe25aa50d1a7a29acb394
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-27 12:11:26 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -249,3 +249,4 @@ function getDivisorsCnt(n) {
 //? console.log(getDivisorsCnt(54), 8);
 
 //+ Round up to the next multiple of 5
+//+ Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
