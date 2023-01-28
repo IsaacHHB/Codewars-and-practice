4582e6ed8087942162a1adaf795d39195973fd63
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-28 13:57:54 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -273,28 +273,3 @@ console.log(roundToNext5(555), 555)
 
 const solution = nums => !nums || !nums.length ? [] : nums.sort((a,b) => a - b)
 
-console.log(solution([1,2,3,10,5]), [1,2,3,5,10])
-console.log(solution(null), [])
-console.log(solution([]), [])
-console.log(solution([20, 2, 10]), [2,10,20])
-console.log(solution([2, 20, 10]), [2,10,20])
-
-//+ Make a function that does arithmetic!
-//+ Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them. a and b will both be positive integers, and a will always be the first number in the operation, and b always the second. The four operators are "add", "subtract", "divide", "multiply".
-
-function arithmetic(a, b, operator){
-    if(operator === "add"){
-      return a + b
-    }else if(operator === "subtract"){
-      return a - b
-    }else if(operator === "multiply"){
-      return a * b
-    }else{
-      return a / b
-    }
-  }
-
-console.log(arithmetic(1, 2, "add"), 3);
-console.log(arithmetic(8, 2, "subtract"), 6);
-console.log(arithmetic(5, 2, "multiply"), 10);
-console.log(arithmetic(8, 2, "divide"), 4);
