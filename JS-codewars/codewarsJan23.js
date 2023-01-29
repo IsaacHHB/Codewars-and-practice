f91a9770ba5ef2cf9a09fe4d6834426e0bb963c3
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-29 11:23:22 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -274,3 +274,4 @@ function roundToNext5(n) {
 const solution = nums => !nums || !nums.length ? [] : nums.sort((a,b) => a - b)
 
 console.log(solution([1,2,3,10,5]), [1,2,3,5,10])
+console.log(solution(null), [])
