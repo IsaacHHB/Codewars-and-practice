e2f4a1652ae6c3f5fc62d42fc0379c54c1e21075
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-30 15:12:08 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -297,4 +297,4 @@ function arithmetic(a, b, operator) {
 //? console.log(arithmetic(1, 2, "add"), 3);
 //? console.log(arithmetic(8, 2, "subtract"), 6);
 //? console.log(arithmetic(5, 2, "multiply"), 10);
-console.log(arithmetic(8, 2, "divide"), 4); 
+//? console.log(arithmetic(8, 2, "divide"), 4); 
