01c95a5fd839f1b3736a47f0a787473517fefb94
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-30 15:11:03 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -279,3 +279,4 @@ const solution = nums => !nums || !nums.length ? [] : nums.sort((a, b) => a - b)
 //? console.log(solution([20, 2, 10]), [2,10,20])
 //? console.log(solution([2, 20, 10]), [2,10,20])
 
+//+ Make a function that does arithmetic!
