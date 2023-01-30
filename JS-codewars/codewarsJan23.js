11de9c0c609c54476077358e46202ec02d963ae2
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-01-30 15:11:09 -0800

    Update codewarsJan23.js

--- JS-codewars/codewarsJan23.js
+++ JS-codewars/codewarsJan23.js
@@ -280,3 +280,5 @@ const solution = nums => !nums || !nums.length ? [] : nums.sort((a, b) => a - b)
 //? console.log(solution([2, 20, 10]), [2,10,20])
 
 //+ Make a function that does arithmetic!
+//+ Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them. a and b will both be positive integers, and a will always be the first number in the operation, and b always the second. The four operators are "add", "subtract", "divide", "multiply".
+
