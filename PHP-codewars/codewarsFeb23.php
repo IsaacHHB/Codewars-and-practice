3f745b167ce4646441e8d429099a8ab38d1655cb
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-02 14:26:28 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -6,7 +6,7 @@ function even_or_odd(int $n): string{
     return $n % 2 == 0 ? "Even" : "Odd";
 }
 
-echo "Even", even_or_odd(2);
+//? echo "Even", even_or_odd(2);
 echo "Even", even_or_odd(0);
 echo "Odd", even_or_odd(7);
 echo "Odd", even_or_odd(1);
