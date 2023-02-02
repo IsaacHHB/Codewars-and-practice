47cbbfcef5a3f6c171626016f307d91df7fb0063
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-02 14:24:56 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -2,7 +2,6 @@
 //+ Even or Odd
 //+ Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-function even_or_odd(int $n): string
-{
+function even_or_odd(int $n): string{
     return $n % 2 == 0 ? "Even" : "Odd";
 }
