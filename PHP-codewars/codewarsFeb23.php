3555aed8475a0e821e83ab20168938f9a0c3162b
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-04 14:23:33 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -52,3 +52,4 @@ echo 15, positive_sum([1, 2, 3, 4, 5]);
 echo 13, positive_sum([1, -2, 3, 4, 5]);
 echo 0, positive_sum([]);
 echo 0, positive_sum([-1, -2, -3, -4, -5]);
+echo 9, positive_sum([-1, 2, 3, 4, -5]);
