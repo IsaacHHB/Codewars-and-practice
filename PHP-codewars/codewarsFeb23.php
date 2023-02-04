eb8fb4deadb76c78a538b04c1cccab9b58bdd5a9
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-04 14:22:21 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -36,3 +36,5 @@ function isVow(array $a){
 //? echo ["u","a",107,"u"], isVow([111,105,101,97]);
 
 //+ Sum of positive
+//+ You get an array of numbers, return the sum of all of the positives ones. Example [1,-4,7,12] => 1 + 7 + 12 = 20 Note: if there is nothing to sum, the sum is default to 0.
+
