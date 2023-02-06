2735c9477b9825c0961b90c60a0b77bb1f938ca4
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-05 16:50:59 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -63,5 +63,5 @@ function numberToString($num){
 
 //? echo '67', numberToString(67);
 //? echo '51', numberToString(51);
-echo '397', numberToString(397);
+//? echo '397', numberToString(397);
 echo '799', numberToString(799);
