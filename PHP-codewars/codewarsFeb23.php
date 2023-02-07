b898322b95bedcb87c65bff22b4bff78512a512c
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-06 21:45:52 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -68,3 +68,5 @@ function numberToString($num){
 //? echo '799', numberToString(799);
 
 //+ Remove First and Last Character
+//+ It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
