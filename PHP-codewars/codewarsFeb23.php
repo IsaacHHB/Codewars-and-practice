4833814fd55d6f0c124b8cd52a07b41f8bec4a80
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-07 21:49:44 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -91,4 +91,4 @@ function remove_char(string $s) : string
 //? echo 'lac', remove_char('place');
 
 //+ Square(n) Sum
-//+ 
+//+ Complete the square sum function so that it squares each number passed into it and then sums the results together. For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
