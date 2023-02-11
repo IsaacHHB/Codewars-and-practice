bbaada0354559c7d69883b8e41cfb05b0ff6235d
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-10 16:54:29 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -111,3 +111,11 @@ function square_sum($numbers) : int {
 
 //+ Grasshopper - Summation
 //+ Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+function summation($n) {
+    $total = 0;
+    for($i = 0; $i <= $n; $i++){
+      $total += $i;
+    }
+    return $total;
+  }
