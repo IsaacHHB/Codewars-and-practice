f894c11cd38de987699c985fa899d688374eb98a
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-11 13:01:09 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -129,3 +129,7 @@ function summation($n) {
 //+ Opposite number
 //+ Very simple, given an integer or a floating-point number, find its opposite.
 
+function opposite($n) {
+    return $n * -1;
+ }
+
