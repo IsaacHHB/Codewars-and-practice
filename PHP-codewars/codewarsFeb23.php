491e27d83657aca86130d03bc5992b7cf81935dc
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-14 13:02:50 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -140,3 +140,7 @@ function opposite($n) {
 //+ String repeat
 //+ Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
 
+function repeatStr($n, $str){
+    return str_repeat($str, $n);
+ }
+
