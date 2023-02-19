b236fdb32021ee50d8eca515673ae16246d820c1
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-19 11:21:08 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -161,3 +161,9 @@ function no_space(string $s): string {
 
 //+ Descending Order
 //+ Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+function descendingOrder(int $n): int {
+    $narr = str_split((string) $n);
+    rsort($narr);
+    return (int) implode('', $narr);
+  }
