0686e54cf618cf55404bc1ebfdfe15682d775f5d
Isaac Hollow Horn Bear <99921497+IsaacHHB@users.noreply.github.com>
2023-02-19 11:20:40 -0800

    Update codewarsFeb23.php

--- PHP-codewars/codewarsFeb23.php
+++ PHP-codewars/codewarsFeb23.php
@@ -158,3 +158,5 @@ function no_space(string $s): string {
 //? echo '8j8mBliB8gimjB8B8jlB', no_space('8 j 8   mBliB8g  imjB8B8  jl  B');
 //? echo '88Bifk8hB8BB8BBBB888chl8BhBfd', no_space('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd');
 //? echo '8aaaaaddddr', no_space('8aaaaa dddd r     ');
+
+//+ Descending Order
